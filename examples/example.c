47120bd8e3d1b47b65e04da4dfb14411cad8b73f
Adrien Plazas <kekun.plazas@laposte.net>
2018-04-16 14:39:59 +0200

    example: Move window title to .ui file
    This avoid hardcoding values when we can put them in the UI description.

--- examples/example.c
+++ examples/example.c
@@ -11,8 +11,6 @@ show_window (GtkApplication *app)
 
   window = example_window_new (app);
 
-  gtk_window_set_title (GTK_WINDOW (window), "Handy Widget Factory");
-
   gtk_widget_show_all (GTK_WIDGET (window));
 }
 
