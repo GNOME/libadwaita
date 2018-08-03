bcd03848aa79d4d3d30cb115cdf6f408fdfa7edb
Guido Günther <agx@sigxcpu.org>
2018-08-03 15:16:03 +0200

    Test flatpak build
    This makes sure the flatpak build actually works and we have
    the examples available for easy download.
    
    This is somewhat based on the GTK+4 flatpak CI test.

---
+++ .gitlab-ci/flatpak-build.sh
@@ -0,0 +1,39 @@
+#!/bin/bash
+
+set -e
+
+builddir=app
+repodir=repo
+appid="$1"
+manifest=examples/${appid}.json
+
+flatpak-builder \
+       --stop-at=libhandy \
+       ${builddir} \
+       ${manifest}
+
+flatpak-builder -v \
+        --run ${builddir} ${manifest} \
+        meson \
+                --prefix /app \
+                --libdir /app/lib \
+                --buildtype debug \
+                -Dintrospection=disabled \
+                -Dvapi=false \
+                _build .
+
+flatpak-builder \
+        --run ${builddir} ${manifest} \
+        ninja -C _build install
+
+flatpak-builder \
+        --finish-only \
+        --repo=${repodir} \
+        ${builddir} \
+        ${manifest}
+
+flatpak build-bundle \
+        ${repodir} \
+        ${appid}-dev.flatpak \
+        --runtime-repo=https://flathub.org/repo/flathub.flatpakrepo \
+        ${appid}
