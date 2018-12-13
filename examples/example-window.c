05603ac8f980a4cec51b7439dd1764cf38837aa4
Adrien Plazas <kekun.plazas@laposte.net>
2018-12-13 09:23:12 +0100

    examples: Use hdy_list_box_separator_header()
    This makes the code simpler.

--- examples/example-window.c
+++ examples/example-window.c
@@ -26,28 +26,6 @@ struct _ExampleWindow
 
 G_DEFINE_TYPE (ExampleWindow, example_window, GTK_TYPE_APPLICATION_WINDOW)
 
-static void
-list_box_separator_header_func (GtkListBoxRow *row,
-                                GtkListBoxRow *before,
-                                gpointer       user_data)
-{
-  GtkWidget *header;
-
-  if (before == NULL) {
-    gtk_list_box_row_set_header (row, NULL);
-
-    return;
-  }
-
-  header = gtk_list_box_row_get_header (row);
-  if (header != NULL)
-    return;
-
-  header = gtk_separator_new (GTK_ORIENTATION_HORIZONTAL);
-  gtk_widget_show (header);
-  gtk_list_box_row_set_header (row, header);
-}
-
 static gboolean
 example_window_key_pressed_cb (GtkWidget     *sender,
                                GdkEventKey   *event,
@@ -326,7 +304,7 @@ static void
 example_window_init (ExampleWindow *self)
 {
   gtk_widget_init_template (GTK_WIDGET (self));
-  gtk_list_box_set_header_func (self->column_listbox, list_box_separator_header_func, NULL, NULL);
+  gtk_list_box_set_header_func (self->column_listbox, hdy_list_box_separator_header, NULL, NULL);
 
   hdy_leaflet_set_visible_child_name (self->content_box, "content");
   update_header_bar (self);
