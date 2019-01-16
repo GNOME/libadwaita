GNOME/libhandy

Implement HdyDialog, an adaptive GtkDialog
https://source.puri.sm/Librem5/libhandy/issues/52

dialog: Start connecting signals

Mostly implements a workaround for tracking the transient parent
Assumes 3.24.2 will include the fix

https://gitlab.gnome.org/GNOME/gtk/merge_requests/424

dialog: Sync dialog & window size

dialog: Adaptive titlebar

For presentation dialogs uses a back button in mobile view
Also enables actions-in-headerbar by default

dialog: Document the new widget

dialog: New sizing rules

Also add more documentation

dialog: Add some simple unit tests

tests/test-dialog.c
@@ -1,0 +1,88 @@
+/*
+ * Copyright © 2018 Zander Brown <zbrown@gnome.org>
+ *
+ * SPDX-License-Identifier: LGPL-2.1+
+ */
+
+#define HANDY_USE_UNSTABLE_API
+#include <handy.h>
+
+gint win_width = 0;
+gint win_height = 0;
+gint dlg_width = 0;
+gint dlg_height = 0;
+
+static void
+win_size_cb (GtkWidget    *widget,
+             GdkRectangle *allocation,
+             gpointer      user_data)
+{
+  gtk_window_get_size (GTK_WINDOW (widget), &win_width, &win_height);
+}
+
+static void
+dlg_size_cb (GtkWidget    *widget,
+             GdkRectangle *allocation,
+             gpointer      user_data)
+{
+  gtk_window_get_size (GTK_WINDOW (widget), &dlg_width, &dlg_height);
+}
+
+static void
+test_hdy_dialog_is_small (void)
+{
+  GtkWidget *window;
+  GtkWidget *dialog;
+
+  win_width = 0;
+  win_height = 0;
+  dlg_width = 0;
+  dlg_height = 0;
+
+  window = gtk_window_new (GTK_WINDOW_TOPLEVEL);
+  gtk_window_resize (GTK_WINDOW (window), 400, 400);
+  g_signal_connect (window, "size-allocate", G_CALLBACK (win_size_cb), NULL);
+  gtk_widget_show (window);
+
+  dialog = hdy_dialog_new (GTK_WINDOW (window));
+  g_signal_connect (dialog, "size-allocate", G_CALLBACK (dlg_size_cb), NULL);
+  gtk_widget_show (dialog);
+
+  g_assert_cmpint (win_height, ==, dlg_height);
+  g_assert_cmpint (win_height, ==, dlg_width);
+}
+
+static void
+test_hdy_dialog_normal (void)
+{
+  GtkWidget *window;
+  GtkWidget *dialog;
+
+  win_width = 0;
+  win_height = 0;
+  dlg_width = 0;
+  dlg_height = 0;
+
+  window = gtk_window_new (GTK_WINDOW_TOPLEVEL);
+  gtk_window_resize (GTK_WINDOW (window), 800, 800);
+  g_signal_connect (window, "size-allocate", G_CALLBACK (win_size_cb), NULL);
+  gtk_widget_show (window);
+
+  dialog = hdy_dialog_new (GTK_WINDOW (window));
+  g_signal_connect (dialog, "size-allocate", G_CALLBACK (dlg_size_cb), NULL);
+  gtk_widget_show (dialog);
+
+  g_assert_cmpint (win_height, !=, dlg_height);
+  g_assert_cmpint (win_height, !=, dlg_width);
+}
+
+gint
+main (gint argc,
+      gchar *argv[])
+{
+  gtk_test_init (&argc, &argv, NULL);
+
+  g_test_add_func("/Handy/Dialog/is_small", test_hdy_dialog_is_small);
+  g_test_add_func("/Handy/Dialog/normal", test_hdy_dialog_normal);
+  return g_test_run();
+}

GNOME/libadwaita

tests/test-dialog.c
@@ -1,0 +1,88 @@
+/*
+ * Copyright © 2018 Zander Brown <zbrown@gnome.org>
+ *
+ * SPDX-License-Identifier: LGPL-2.1+
+ */
+
+#define HANDY_USE_UNSTABLE_API
+#include <handy.h>
+
+gint win_width = 0;
+gint win_height = 0;
+gint dlg_width = 0;
+gint dlg_height = 0;
+
+static void
+win_size_cb (GtkWidget    *widget,
+             GdkRectangle *allocation,
+             gpointer      user_data)
+{
+  gtk_window_get_size (GTK_WINDOW (widget), &win_width, &win_height);
+}
+
+static void
+dlg_size_cb (GtkWidget    *widget,
+             GdkRectangle *allocation,
+             gpointer      user_data)
+{
+  gtk_window_get_size (GTK_WINDOW (widget), &dlg_width, &dlg_height);
+}
+
+static void
+test_hdy_dialog_is_small (void)
+{
+  GtkWidget *window;
+  GtkWidget *dialog;
+
+  win_width = 0;
+  win_height = 0;
+  dlg_width = 0;
+  dlg_height = 0;
+
+  window = gtk_window_new (GTK_WINDOW_TOPLEVEL);
+  gtk_window_resize (GTK_WINDOW (window), 400, 400);
+  g_signal_connect (window, "size-allocate", G_CALLBACK (win_size_cb), NULL);
+  gtk_widget_show (window);
+
+  dialog = hdy_dialog_new (GTK_WINDOW (window));
+  g_signal_connect (dialog, "size-allocate", G_CALLBACK (dlg_size_cb), NULL);
+  gtk_widget_show (dialog);
+
+  g_assert_cmpint (win_height, ==, dlg_height);
+  g_assert_cmpint (win_width, ==, dlg_width);
+}
+
+static void
+test_hdy_dialog_normal (void)
+{
+  GtkWidget *window;
+  GtkWidget *dialog;
+
+  win_width = 0;
+  win_height = 0;
+  dlg_width = 0;
+  dlg_height = 0;
+
+  window = gtk_window_new (GTK_WINDOW_TOPLEVEL);
+  gtk_window_resize (GTK_WINDOW (window), 800, 800);
+  g_signal_connect (window, "size-allocate", G_CALLBACK (win_size_cb), NULL);
+  gtk_widget_show (window);
+
+  dialog = hdy_dialog_new (GTK_WINDOW (window));
+  g_signal_connect (dialog, "size-allocate", G_CALLBACK (dlg_size_cb), NULL);
+  gtk_widget_show (dialog);
+
+  g_assert_cmpint (win_height, !=, dlg_height);
+  g_assert_cmpint (win_width, !=, dlg_width);
+}
+
+gint
+main (gint argc,
+      gchar *argv[])
+{
+  gtk_test_init (&argc, &argv, NULL);
+
+  g_test_add_func("/Handy/Dialog/is_small", test_hdy_dialog_is_small);
+  g_test_add_func("/Handy/Dialog/normal", test_hdy_dialog_normal);
+  return g_test_run();
+}