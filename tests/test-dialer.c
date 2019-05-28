01c1c4961588ae6129262991a5f85c3af7e2c378
Guido Günther <agx@sigxcpu.org>
2019-05-28 09:48:56 +0200

    tests: Test hdy_dialer_{set,get}_show_action_button()

--- tests/test-dialer.c
+++ tests/test-dialer.c
@@ -68,6 +68,35 @@ test_hdy_dialer_clear_number (void)
 }
 
 
+static void
+test_hdy_dialer_action_buttons (void)
+{
+  HdyDialer *dialer = HDY_DIALER (hdy_dialer_new ());
+  gboolean val;
+
+  notified = 0;
+  g_signal_connect (dialer, "notify::show-action-buttons", G_CALLBACK (notify_cb), NULL);
+
+  /* Getters/setters */
+  g_assert_false (hdy_dialer_get_show_action_buttons (dialer));
+  hdy_dialer_set_show_action_buttons (dialer, TRUE);
+  g_assert_true (hdy_dialer_get_show_action_buttons (dialer));
+  hdy_dialer_set_show_action_buttons (dialer, FALSE);
+  g_assert_false (hdy_dialer_get_show_action_buttons (dialer));
+  g_assert_cmpint (notified, ==, 2);
+
+  /* Property */
+  g_object_set (dialer, "show-action-buttons", TRUE, NULL);
+  g_object_get (dialer, "show-action-buttons", &val, NULL);
+  g_assert_true (val);
+  g_assert_cmpint (notified, ==, 3);
+
+  /* Setting the same value should not notify */
+  hdy_dialer_set_show_action_buttons (dialer, TRUE);
+  g_assert_cmpint (notified, ==, 3);
+}
+
+
 gint
 main (gint argc,
       gchar *argv[])
@@ -77,5 +106,6 @@ main (gint argc,
 
   g_test_add_func ("/Handy/Dialer/setnumber", test_hdy_dialer_setnumber);
   g_test_add_func ("/Handy/Dialer/clear_number", test_hdy_dialer_clear_number);
+  g_test_add_func ("/Handy/Dialer/action_buttons", test_hdy_dialer_action_buttons);
   return g_test_run ();
 }
