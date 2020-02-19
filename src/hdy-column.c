46d77aa8d32108881dd123ea34c318ac1c8d232d
Adrien Plazas <kekun.plazas@laposte.net>
2020-02-19 13:51:23 +0100

    column: Fix the CSS node name documentation
    The hdy prefix was removed in a previous commit.

--- src/hdy-column.c
+++ src/hdy-column.c
@@ -26,7 +26,7 @@
  *
  * # CSS nodes
  *
- * #HdyColumn has a single CSS node with name hdycolumn. The node will get the
+ * #HdyColumn has a single CSS node with name column. The node will get the
  * style classes .wide when its child reached its maximum width, .narrow when
  * the column allocates its full width to its child, .medium in-between, or none
  * if it didn't compute its size yet.
