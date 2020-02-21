f1ab7f721811d90f628ef083f244ac586d5cf92b
Adrien Plazas <kekun.plazas@laposte.net>
2020-02-21 08:22:09 +0100

    leaflet: Avoid divisions by 0

--- src/hdy-leaflet.c
+++ src/hdy-leaflet.c
@@ -1903,13 +1903,13 @@ hdy_leaflet_size_allocate_unfolded (GtkWidget     *widget,
 
   if (box_homogeneous) {
     if (orientation == GTK_ORIENTATION_HORIZONTAL) {
-      homogeneous_size = allocation->width / n_visible_children;
-      n_expand_children = allocation->width % n_visible_children;
+      homogeneous_size = n_visible_children > 0 ? allocation->width / n_visible_children : 0;
+      n_expand_children = n_visible_children > 0 ? allocation->width % n_visible_children : 0;
       min_size = allocation->width - n_expand_children;
     }
     else {
-      homogeneous_size = allocation->height / n_visible_children;
-      n_expand_children = allocation->height % n_visible_children;
+      homogeneous_size = n_visible_children > 0 ? allocation->height / n_visible_children : 0;
+      n_expand_children = n_visible_children > 0 ? allocation->height % n_visible_children : 0;
       min_size = allocation->height - n_expand_children;
     }
   }
