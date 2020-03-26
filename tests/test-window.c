3edb06c79a601310095f2d8d44c4f960be3c0a67
Alexander Mikhaylenko <alexm@gnome.org>
2020-03-27 03:31:34 +0500

    tests: Add a HdyWindow test
    Signed-off-by: Alexander Mikhaylenko <alexm@gnome.org>

---
+++ tests/test-window.c
@@ -0,0 +1,30 @@
+/*
+ * Copyright (C) 2020 Alexander Mikhaylenko <alexm@gnome.org>
+ *
+ * SPDX-License-Identifier: LGPL-2.1+
+ */
+
+#define HANDY_USE_UNSTABLE_API
+#include <handy.h>
+
+
+static void
+test_hdy_window_new (void)
+{
+  g_autoptr (GtkWidget) window = NULL;
+
+  window = g_object_ref_sink (hdy_window_new ());
+  g_assert_nonnull (window);
+}
+
+
+gint
+main (gint argc,
+      gchar *argv[])
+{
+  gtk_test_init (&argc, &argv, NULL);
+
+  g_test_add_func("/Handy/Window/new", test_hdy_window_new);
+
+  return g_test_run();
+}
