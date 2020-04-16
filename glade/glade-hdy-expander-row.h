b04befb820317229e325bf32dc903e9d800496ec
Alexander Mikhaylenko <alexm@gnome.org>
2020-04-16 22:41:18 +0500

    glade: Support HdyExpanderRow
    Signed-off-by: Alexander Mikhaylenko <alexm@gnome.org>

---
+++ glade/glade-hdy-expander-row.h
@@ -0,0 +1,40 @@
+/*
+ * Copyright (C) 2020 Alexander Mikhaylenko <alexm@gnome.org>
+ *
+ * SPDX-License-Identifier: LGPL-2.1+
+ */
+
+#pragma once
+
+#include <gladeui/glade.h>
+
+#define HANDY_USE_UNSTABLE_API
+#include <handy.h>
+
+void glade_hdy_expander_row_post_create (GladeWidgetAdaptor *adaptor,
+                                         GObject            *container,
+                                         GladeCreateReason   reason);
+
+void glade_hdy_expander_row_get_child_property (GladeWidgetAdaptor *adaptor,
+                                                GObject            *container,
+                                                GObject            *child,
+                                                const gchar        *property_name,
+                                                GValue             *value);
+void glade_hdy_expander_row_set_child_property (GladeWidgetAdaptor *adaptor,
+                                                GObject            *container,
+                                                GObject            *child,
+                                                const gchar        *property_name,
+                                                GValue             *value);
+
+void glade_hdy_expander_row_add_child (GladeWidgetAdaptor *adaptor,
+                                       GObject            *object,
+                                       GObject            *child);
+
+void glade_hdy_expander_row_remove_child (GladeWidgetAdaptor *adaptor,
+                                          GObject            *object,
+                                          GObject            *child);
+
+gboolean glade_hdy_expander_row_add_verify (GladeWidgetAdaptor *adaptor,
+                                            GtkWidget          *object,
+                                            GtkWidget          *child,
+                                            gboolean            user_feedback);
