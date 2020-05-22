59249eee508cab1e9caff71fc8b5d2c0be676608
Alexander Mikhaylenko <alexm@gnome.org>
2020-05-22 16:20:20 +0500

    ci: Speed up api-visibility job
    Stop using find, and use default image and empty before_script.
    
    Signed-off-by: Alexander Mikhaylenko <alexm@gnome.org>

--- lint/api-visibility.sh
+++ lint/api-visibility.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 
 # Check that private headers aren't included in public ones.
-if grep "include.*private.h" $(find src -name '*\.h' -not -regex '.*private.h');
+if grep "include.*private.h" $(ls src/*.h | grep -v "private.h");
 then
   echo "Private headers shouldn't be included in public ones."
   exit 1
 fi
 
 # Check that handy.h contains all the public headers.
-for header in $(find src -name '*.h' -not -regex '.*private.h' -not -regex '.*/handy.h');
+for header in $(ls src | grep "\.h$" | grep -v "private.h" | grep -v handy.h);
 do
   if ! grep -q "$(basename $header)" src/handy.h;
   then
