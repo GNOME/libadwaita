0e2770b417dbfcb904f2f33c2e4c21e2cc4d3dab
Alexander Mikhaylenko <alexm@gnome.org>
2020-05-25 00:58:57 +0500

    build: Add an override for pygobject
    PyGObject seems to load libraries lazily, which means the constructor won't
    run until after we've referenced a libhandy symbol. However, this won't
    work if a type is first referenced from a GtkBuilder file, so we need to
    make sure the constructor runs before that. Add an override to preload it.
    
    Thanks to Zander Brown for explanations.
    
    Signed-off-by: Alexander Mikhaylenko <alexm@gnome.org>

---
+++ src/Handy.py
@@ -0,0 +1,7 @@
+# Copyright (C) 2020 Alexander Mikhaylenko <alexm@gnome.org>
+#
+# SPDX-License-Identifier: LGPL-2.1+
+
+from gi.repository import GObject, Handy
+
+GObject.type_ensure(Handy.ActionRow)
