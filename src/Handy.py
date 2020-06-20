ccaec16d622d26cb6936f4e4a1972a9fb79be0ec
Adrien Plazas <kekun.plazas@laposte.net>
2020-06-20 10:27:20 +0200

    build: Drop the override for pygobject
    It was a workaround for the library constructor being called too late in
    the pygobject bindings. It isn't needed as the library constructor has
    been replaced by hdy_init().

--- src/Handy.py
+++
@@ -1,7 +0,0 @@
-# Copyright (C) 2020 Alexander Mikhaylenko <alexm@gnome.org>
-#
-# SPDX-License-Identifier: LGPL-2.1+
-
-from gi.repository import GObject, Handy
-
-GObject.type_ensure(Handy.ActionRow)
