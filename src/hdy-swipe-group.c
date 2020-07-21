e00cc12a2ae4d23e66fb637f81e2cf2098d1f2c1
Alexander Mikhaylenko <alexm@gnome.org>
2020-07-22 02:27:55 +0500

    swipe-group: Remove an unnecessary include
    It's already in hdy-swipe-group.h
    
    Signed-off-by: Alexander Mikhaylenko <alexm@gnome.org>

--- src/hdy-swipe-group.c
+++ src/hdy-swipe-group.c
@@ -9,7 +9,6 @@
 #include "hdy-swipe-group.h"
 #include <gtk/gtk.h>
 #include "hdy-navigation-direction.h"
-#include "hdy-swipeable.h"
 #include "hdy-swipe-tracker-private.h"
 
 #define BUILDABLE_TAG_OBJECT "object"
