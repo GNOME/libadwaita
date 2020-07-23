9f475d413b2f4cb4af14408ef288215344e903cc
Adrien Plazas <kekun.plazas@laposte.net>
2020-07-23 10:18:18 +0200

    demo: Add subpages to the preferences window

--- examples/hdy-demo-preferences-window.c
+++ examples/hdy-demo-preferences-window.c
@@ -3,6 +3,9 @@
 struct _HdyDemoPreferencesWindow
 {
   HdyPreferencesWindow parent_instance;
+
+  GtkWidget *subpage1;
+  GtkWidget *subpage2;
 };
 
 G_DEFINE_TYPE (HdyDemoPreferencesWindow, hdy_demo_preferences_window, HDY_TYPE_PREFERENCES_WINDOW)
@@ -14,11 +17,36 @@ hdy_demo_preferences_window_new (void)
 }
 
 static void
+return_to_preferences_cb (HdyDemoPreferencesWindow *self)
+{
+  hdy_preferences_window_close_subpage (HDY_PREFERENCES_WINDOW (self));
+}
+
+static void
+subpage1_activated_cb (HdyDemoPreferencesWindow *self)
+{
+  hdy_preferences_window_present_subpage (HDY_PREFERENCES_WINDOW (self), self->subpage1);
+}
+
+static void
+subpage2_activated_cb (HdyDemoPreferencesWindow *self)
+{
+  hdy_preferences_window_present_subpage (HDY_PREFERENCES_WINDOW (self), self->subpage2);
+}
+
+static void
 hdy_demo_preferences_window_class_init (HdyDemoPreferencesWindowClass *klass)
 {
   GtkWidgetClass *widget_class = GTK_WIDGET_CLASS (klass);
 
   gtk_widget_class_set_template_from_resource (widget_class, "/sm/puri/Handy/Demo/ui/hdy-demo-preferences-window.ui");
+
+  gtk_widget_class_bind_template_child (widget_class, HdyDemoPreferencesWindow, subpage1);
+  gtk_widget_class_bind_template_child (widget_class, HdyDemoPreferencesWindow, subpage2);
+
+  gtk_widget_class_bind_template_callback (widget_class, return_to_preferences_cb);
+  gtk_widget_class_bind_template_callback (widget_class, subpage1_activated_cb);
+  gtk_widget_class_bind_template_callback (widget_class, subpage2_activated_cb);
 }
 
 static void
