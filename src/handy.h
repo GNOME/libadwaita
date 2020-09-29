6d03f0a45b9c6b6abbc05ba878047ef2b9042ac6
Alexander Mikhaylenko <alexm@gnome.org>
2020-09-29 22:37:04 +0500

    Uncomment HdyValueObject

--- src/handy.h
+++ src/handy.h
@@ -66,8 +66,8 @@ G_BEGIN_DECLS
 #include "hdy-swipeable.h"
 #include "hdy-title-bar.h"
 #include "hdy-types.h"
-#include "hdy-value-object.h"
 */
+#include "hdy-value-object.h"
 #include "hdy-view-switcher.h"
 #include "hdy-view-switcher-bar.h"
 #include "hdy-view-switcher-title.h"
