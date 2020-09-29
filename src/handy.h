b15dd92b886ab76568e3914ec8596a70cfba245d
Alexander Mikhaylenko <alexm@gnome.org>
2020-09-30 03:11:18 +0500

    Uncomment HdyNavigationDirection

--- src/handy.h
+++ src/handy.h
@@ -47,9 +47,7 @@ G_BEGIN_DECLS
 #include "hdy-leaflet.h"
 */
 #include "hdy-main.h"
-/*
 #include "hdy-navigation-direction.h"
-*/
 #include "hdy-preferences-group.h"
 #include "hdy-preferences-page.h"
 #include "hdy-preferences-row.h"
