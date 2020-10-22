50ef08661a321ab1577d5dc333347f68ebfb0f08
Alexander Mikhaylenko <alexm@gnome.org>
2020-10-22 19:01:10 +0500

    enum-value-object: Add properties

--- src/hdy-enum-value-object.c
+++ src/hdy-enum-value-object.c
@@ -5,6 +5,7 @@
  */
 
 #include "config.h"
+#include <glib/gi18n-lib.h>
 
 #include "hdy-enum-value-object.h"
 
@@ -26,7 +27,76 @@ struct _HdyEnumValueObject
   GEnumValue enum_value;
 };
 
+enum {
+  PROP_0,
+  PROP_VALUE,
+  PROP_NAME,
+  PROP_NICK,
+  LAST_PROP,
+};
+
+static GParamSpec *props[LAST_PROP];
+
 G_DEFINE_TYPE (HdyEnumValueObject, hdy_enum_value_object, G_TYPE_OBJECT)
+
+static void
+hdy_enum_value_object_get_property (GObject    *object,
+                                    guint       prop_id,
+                                    GValue     *value,
+                                    GParamSpec *pspec)
+{
+  HdyEnumValueObject *self = HDY_ENUM_VALUE_OBJECT (object);
+
+  switch (prop_id) {
+  case PROP_VALUE:
+    g_value_set_int (value, hdy_enum_value_object_get_value (self));
+    break;
+  case PROP_NAME:
+    g_value_set_string (value, hdy_enum_value_object_get_name (self));
+    break;
+  case PROP_NICK:
+    g_value_set_string (value, hdy_enum_value_object_get_nick (self));
+    break;
+  default:
+    G_OBJECT_WARN_INVALID_PROPERTY_ID (object, prop_id, pspec);
+  }
+}
+
+static void
+hdy_enum_value_object_class_init (HdyEnumValueObjectClass *klass)
+{
+  GObjectClass *object_class = G_OBJECT_CLASS (klass);
+
+  object_class->get_property = hdy_enum_value_object_get_property;
+
+  props[PROP_VALUE] =
+    g_param_spec_int ("value",
+                      _("Value"),
+                      _("The enum object value"),
+                      G_MININT, G_MAXINT, 0,
+                      G_PARAM_READABLE);
+
+  props[PROP_NAME] =
+    g_param_spec_string ("name",
+                         _("Name"),
+                         _("The enum object name"),
+                         NULL,
+                         G_PARAM_READABLE);
+
+  props[PROP_NICK] =
+    g_param_spec_string ("nick",
+                         _("Nick"),
+                         _("The enum object nick"),
+                         NULL,
+                         G_PARAM_READABLE);
+
+  g_object_class_install_properties (object_class, LAST_PROP, props);
+}
+
+static void
+hdy_enum_value_object_init (HdyEnumValueObject *self)
+{
+}
 
 HdyEnumValueObject *
 hdy_enum_value_object_new (GEnumValue *enum_value)
@@ -36,16 +106,6 @@ hdy_enum_value_object_new (GEnumValue *enum_value)
   self->enum_value = *enum_value;
 
   return self;
-}
-
-static void
-hdy_enum_value_object_class_init (HdyEnumValueObjectClass *klass)
-{
-}
-
-static void
-hdy_enum_value_object_init (HdyEnumValueObject *self)
-{
 }
 
 gint
