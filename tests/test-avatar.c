44768ad50df8a693810367fb89d4e8aebb30d90a
Julian Sparber <julian@sparber.net>
2020-11-02 13:55:17 +0100

    test: Add test for hdy_avatar_draw_to_pixbuf ()

--- tests/test-avatar.c
+++ tests/test-avatar.c
@@ -201,6 +201,19 @@ test_hdy_avatar_custom_image (void)
   g_object_unref (avatar);
 }
 
+static void
+test_hdy_avatar_draw_to_pixbuf (void)
+{
+  g_autoptr (HdyAvatar) avatar = NULL;
+  g_autoptr (GdkPixbuf) pixbuf = NULL;
+
+  avatar = g_object_ref_sink (HDY_AVATAR (hdy_avatar_new (TEST_SIZE, NULL, TRUE)));
+
+  pixbuf = hdy_avatar_draw_to_pixbuf (avatar, TEST_SIZE * 2, 1);
+
+  g_assert_cmpint (gdk_pixbuf_get_width (pixbuf), ==, TEST_SIZE * 2);
+  g_assert_cmpint (gdk_pixbuf_get_height (pixbuf), ==, TEST_SIZE * 2);
+}
 
 gint
 main (gint argc,
@@ -214,6 +227,7 @@ main (gint argc,
   g_test_add_func ("/Handy/Avatar/icon_name", test_hdy_avatar_icon_name);
   g_test_add_func ("/Handy/Avatar/text", test_hdy_avatar_text);
   g_test_add_func ("/Handy/Avatar/size", test_hdy_avatar_size);
+  g_test_add_func ("/Handy/Avatar/draw_to_pixbuf", test_hdy_avatar_draw_to_pixbuf);
 
   return g_test_run ();
 }
