20921c8e27cceffa7816e5ccfe645568a1c164ac
Adrien Plazas <kekun.plazas@laposte.net>
2020-11-16 10:29:22 +0100

    Initial version of the site

---
+++ doc.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+DOC_DIR=public/doc/
+REFS="
+master
+"
+
+IFS='
+'
+
+mkdir -p $DOC_DIR
+
+for REF in $REFS; do
+  API_VERSION=`echo $REF | sed 's/libhandy-\([0-9][0-9]*\)-\([0-9][0-9]*\)/\1.\2/'`
+
+  curl -L --output "$REF.zip" "https://gitlab.gnome.org/GNOME/libhandy/-/jobs/artifacts/$REF/download?job=build-gtkdoc"
+  unzip -d "$REF" "$REF.zip"
+  mv "$REF/_reference" $DOC_DIR/$API_VERSION
+
+  rm "$REF.zip"
+  rm -rf "$REF"
+done
