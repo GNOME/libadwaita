98f5b47198ef6354292999414938da47f1e28023
Adrien Plazas <kekun.plazas@laposte.net>
2020-11-16 12:54:23 +0100

    Add a documentation page
    It links to the documentations of:
    - the latest 1.x version
    - the 1.0 version
    - the master version

--- doc.sh
+++ doc.sh
@@ -3,7 +3,10 @@
 DOC_DIR=public/doc/
 REFS="
 master
+libhandy-1-0
 "
+
+LATEST_STABLE_1=1.0
 
 IFS='
 '
@@ -12,6 +15,7 @@ mkdir -p $DOC_DIR
 
 for REF in $REFS; do
   API_VERSION=`echo $REF | sed 's/libhandy-\([0-9][0-9]*\)-\([0-9][0-9]*\)/\1.\2/'`
+  API_VERSION=`echo $API_VERSION | sed 's/v0.0.\([0-9][0-9]*\)/0.0.\1/'`
 
   curl -L --output "$REF.zip" "https://gitlab.gnome.org/GNOME/libhandy/-/jobs/artifacts/$REF/download?job=build-gtkdoc"
   unzip -d "$REF" "$REF.zip"
@@ -20,3 +24,5 @@ for REF in $REFS; do
   rm "$REF.zip"
   rm -rf "$REF"
 done
+
+cp -r $DOC_DIR/$LATEST_STABLE_1 $DOC_DIR/1-latest
