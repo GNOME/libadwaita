4a3b59662d7c21ba86ccbb0f0d2c85258769485f
Adrien Plazas <kekun.plazas@laposte.net>
2020-11-23 09:38:13 +0100

    doc: Cross link to our dependencies
    This avoids to have invalid links in the doc.

--- doc.sh
+++ doc.sh
@@ -25,3 +25,11 @@ for REF in $REFS; do
 done
 
 cp -r $DOC_DIR/$LATEST_STABLE_1 $DOC_DIR/1-latest
+
+find $DOC_DIR -type f -print0 | xargs -0 sed -i 's|\.\./gdk3/|https://developer.gnome.org/gdk3/stable/|g'
+find $DOC_DIR -type f -print0 | xargs -0 sed -i 's|\.\./gdk-pixbuf/|https://developer.gnome.org/gdk-pixbuf/stable/|g'
+find $DOC_DIR -type f -print0 | xargs -0 sed -i 's|\.\./gio/|https://developer.gnome.org/gio/stable/|g'
+find $DOC_DIR -type f -print0 | xargs -0 sed -i 's|\.\./glib/|https://developer.gnome.org/glib/stable/|g'
+find $DOC_DIR -type f -print0 | xargs -0 sed -i 's|\.\./gobject/|https://developer.gnome.org/gobject/stable/|g'
+find $DOC_DIR -type f -print0 | xargs -0 sed -i 's|\.\./gtk3/|https://developer.gnome.org/gtk3/stable/|g'
+find $DOC_DIR -type f -print0 | xargs -0 sed -i 's|\.\./pango/|https://developer.gnome.org/pango/stable/|g'
