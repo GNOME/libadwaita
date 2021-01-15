5f4d636056fe7a1b6f9c471007114259dc88fcf1
Alexander Mikhaylenko <alexm@gnome.org>
2021-01-15 12:06:19 +0500

    Rename to libadwaita

--- doc.sh
+++ doc.sh
@@ -3,7 +3,6 @@
 DOC_DIR=public/doc
 REFS="
 master
-libhandy-1-0
 "
 
 LATEST_STABLE_1=1.0
@@ -14,9 +13,9 @@ IFS='
 mkdir -p $DOC_DIR
 
 for REF in $REFS; do
-  API_VERSION=`echo $REF | sed 's/libhandy-\([0-9][0-9]*\)-\([0-9][0-9]*\)/\1.\2/'`
+  API_VERSION=`echo $REF | sed 's/libadwaita-\([0-9][0-9]*\)-\([0-9][0-9]*\)/\1.\2/'`
 
-  curl -L --output "$REF.zip" "https://gitlab.gnome.org/GNOME/libhandy/-/jobs/artifacts/$REF/download?job=build-gtkdoc"
+  curl -L --output "$REF.zip" "https://gitlab.gnome.org/exalm/libadwaita/-/jobs/artifacts/$REF/download?job=build-gtkdoc"
   unzip -d "$REF" "$REF.zip"
   mv "$REF/_reference" $DOC_DIR/$API_VERSION
 
@@ -26,10 +25,11 @@ done
 
 cp -r $DOC_DIR/$LATEST_STABLE_1 $DOC_DIR/1-latest
 
-find $DOC_DIR -type f -print0 | xargs -0 sed -i 's|\.\./gdk3/|https://developer.gnome.org/gdk3/stable/|g'
+find $DOC_DIR -type f -print0 | xargs -0 sed -i 's|\.\./gdk4/|https://developer.gnome.org/gdk4/stable/|g'
+find $DOC_DIR -type f -print0 | xargs -0 sed -i 's|\.\./gsk4/|https://developer.gnome.org/gsk4/stable/|g'
 find $DOC_DIR -type f -print0 | xargs -0 sed -i 's|\.\./gdk-pixbuf/|https://developer.gnome.org/gdk-pixbuf/stable/|g'
 find $DOC_DIR -type f -print0 | xargs -0 sed -i 's|\.\./gio/|https://developer.gnome.org/gio/stable/|g'
 find $DOC_DIR -type f -print0 | xargs -0 sed -i 's|\.\./glib/|https://developer.gnome.org/glib/stable/|g'
 find $DOC_DIR -type f -print0 | xargs -0 sed -i 's|\.\./gobject/|https://developer.gnome.org/gobject/stable/|g'
-find $DOC_DIR -type f -print0 | xargs -0 sed -i 's|\.\./gtk3/|https://developer.gnome.org/gtk3/stable/|g'
+find $DOC_DIR -type f -print0 | xargs -0 sed -i 's|\.\./gtk4/|https://developer.gnome.org/gtk4/stable/|g'
 find $DOC_DIR -type f -print0 | xargs -0 sed -i 's|\.\./pango/|https://developer.gnome.org/pango/stable/|g'
