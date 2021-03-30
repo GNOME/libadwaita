b9be731c44fb397d6c7552f4bb704e3abc655323
Alexander Mikhaylenko <alexm@gnome.org>
2021-03-30 18:45:11 +0500

    Update the repo URL

--- doc.sh
+++ doc.sh
@@ -15,7 +15,7 @@ mkdir -p $DOC_DIR
 for REF in $REFS; do
   API_VERSION=`echo $REF | sed 's/libadwaita-\([0-9][0-9]*\)-\([0-9][0-9]*\)/\1.\2/'`
 
-  curl -L --output "$REF.zip" "https://gitlab.gnome.org/exalm/libadwaita/-/jobs/artifacts/$REF/download?job=doc"
+  curl -L --output "$REF.zip" "https://gitlab.gnome.org/GNOME/libadwaita/-/jobs/artifacts/$REF/download?job=doc"
   unzip -d "$REF" "$REF.zip"
   mv "$REF/_doc" $DOC_DIR/$API_VERSION
 
