616731fd8807bc79b0b77888860ee8f7b7a7b023
Alexander Mikhaylenko <alexm@gnome.org>
2021-04-02 22:16:59 +0500

    Import Adwaita stylesheet
    Install it into /org/gtk/libgtk/theme so that GTK can find it. Make
    HighContrast and HighContrastInverse into :hc and :hc-dark variants, alias
    each variant as Adwaita-hc etc themes.
    
    Ensure Adwaita is loaded instead of the default, take care to load
    Adwaita-hc when HighContrast is selected system-wide.
    
    Use Adwaita-hc-dark as a dark variant of the high contrast mode.
    
    Don't integrate the previous libadwaita styles into the stylesheet yet,
    that can be done separately and this is a big change as is.
    
    Fixes https://gitlab.gnome.org/GNOME/libadwaita/-/issues/91

--- src/adw-main.c
+++ src/adw-main.c
@@ -24,131 +24,84 @@ static int adw_initialized = FALSE;
  * Since: 1.0
  */
 
-/* The style provider priority to use for libadwaita widgets custom styling. It
- * is higher than themes and settings, allowing to override theme defaults, but
- * lower than applications and user provided styles, so application developers
- * can nonetheless apply custom styling on top of it. */
-#define ADW_STYLE_PROVIDER_PRIORITY_OVERRIDE (GTK_STYLE_PROVIDER_PRIORITY_SETTINGS + 1)
-
-#define ADW_THEMES_PATH "/org/gnome/Adwaita/themes/"
-
-static inline gboolean
-adw_resource_exists (const char *resource_path)
+static gboolean
+is_high_contrast (void)
 {
-  return g_resources_get_info (resource_path, G_RESOURCE_LOOKUP_FLAGS_NONE, NULL, NULL, NULL);
-}
+  GdkDisplay *display = gdk_display_get_default ();
+  g_auto (GValue) value = G_VALUE_INIT;
+  const char *theme_name;
 
-static char *
-adw_themes_get_theme_name (gboolean *prefer_dark_theme)
-{
-  char *theme_name = NULL;
-  char *p;
+  g_value_init (&value, G_TYPE_STRING);
+  if (!gdk_display_get_setting (display, "gtk-theme-name", &value))
+    return FALSE;
 
-  g_assert (prefer_dark_theme);
+  theme_name = g_value_get_string (&value);
 
-  theme_name = g_strdup (g_getenv ("GTK_THEME"));
-
-  if (theme_name == NULL) {
-    g_object_get (gtk_settings_get_default (),
-                  "gtk-theme-name", &theme_name,
-                  "gtk-application-prefer-dark-theme", prefer_dark_theme,
-                  NULL);
-
-    return theme_name;
-  }
-
-  /* Theme variants are specified with the syntax
-   * "<theme>:<variant>" e.g. "Adwaita:dark" */
-  if (NULL != (p = strrchr (theme_name, ':'))) {
-    *p = '\0';
-    p++;
-    *prefer_dark_theme = g_strcmp0 (p, "dark") == 0;
-  }
-
-  return theme_name;
+  /* TODO: HighContrast shouldn't be a theme */
+  return !g_strcmp0 (theme_name, "HighContrast") ||
+         !g_strcmp0 (theme_name, "HighContrastInverse");
 }
 
 static void
-adw_themes_update (GtkCssProvider *css_provider)
+update_theme (void)
 {
-  g_autofree char *theme_name = NULL;
-  g_autofree char *resource_path = NULL;
-  gboolean prefer_dark_theme = FALSE;
+  GtkSettings *settings = gtk_settings_get_default ();
+  g_autofree char *new_theme_name = NULL;
+  gboolean prefer_dark_theme;
+  const char *variant;
 
-  g_assert (GTK_IS_CSS_PROVIDER (css_provider));
+  g_object_get (settings,
+                "gtk-application-prefer-dark-theme", &prefer_dark_theme,
+                NULL);
 
-  theme_name = adw_themes_get_theme_name (&prefer_dark_theme);
+  if (is_high_contrast ())
+    variant = prefer_dark_theme ? "hc-dark" : "hc";
+  else
+    variant = prefer_dark_theme ? "dark" : "light";
 
-  /* First check with full path to theme+variant */
-  resource_path = g_strdup_printf (ADW_THEMES_PATH"%s%s.css",
-                                   theme_name, prefer_dark_theme ? "-dark" : "");
+  new_theme_name = g_strdup_printf ("Adwaita-%s", variant);
 
-  if (!adw_resource_exists (resource_path)) {
-    /* Now try without the theme variant */
-    g_free (resource_path);
-    resource_path = g_strdup_printf (ADW_THEMES_PATH"%s.css", theme_name);
-
-    if (!adw_resource_exists (resource_path)) {
-      /* Now fallback to shared styling */
-      g_free (resource_path);
-      resource_path = g_strdup (ADW_THEMES_PATH"shared.css");
-
-      g_assert (adw_resource_exists (resource_path));
-    }
-  }
-
-  gtk_css_provider_load_from_resource (css_provider, resource_path);
+  g_object_set (settings, "gtk-theme-name", new_theme_name, NULL);
 }
 
 static void
-load_fallback_style (void)
+setting_changed_cb (GdkDisplay *display,
+                    const char *setting)
 {
-  g_autoptr (GtkCssProvider) css_provider = NULL;
-
-  css_provider = gtk_css_provider_new ();
-  gtk_style_context_add_provider_for_display (gdk_display_get_default (),
-                                              GTK_STYLE_PROVIDER (css_provider),
-                                              GTK_STYLE_PROVIDER_PRIORITY_FALLBACK);
-
-  gtk_css_provider_load_from_resource (css_provider, ADW_THEMES_PATH"fallback.css");
+  if (!g_strcmp0 (setting, "gtk-theme-name"))
+    update_theme ();
 }
 
-/**
- * adw_style_init:
- *
- * Initializes the style classes. This must be called once GTK has been
- * initialized.
- *
- * Since: 1.0
- */
 static void
 adw_style_init (void)
 {
   static gsize guard = 0;
-  g_autoptr (GtkCssProvider) css_provider = NULL;
   GtkSettings *settings;
 
   if (!g_once_init_enter (&guard))
     return;
 
-  css_provider = gtk_css_provider_new ();
-  gtk_style_context_add_provider_for_display (gdk_display_get_default (),
-                                              GTK_STYLE_PROVIDER (css_provider),
-                                              ADW_STYLE_PROVIDER_PRIORITY_OVERRIDE);
-
   settings = gtk_settings_get_default ();
-  g_signal_connect_swapped (settings,
-                           "notify::gtk-theme-name",
-                           G_CALLBACK (adw_themes_update),
-                           css_provider);
-  g_signal_connect_swapped (settings,
-                           "notify::gtk-application-prefer-dark-theme",
-                           G_CALLBACK (adw_themes_update),
-                           css_provider);
 
-  adw_themes_update (css_provider);
+  if (!settings) {
+    g_once_init_leave (&guard, 1);
 
-  load_fallback_style ();
+    return;
+  }
+
+  g_signal_connect (settings,
+                    "notify::gtk-application-prefer-dark-theme",
+                    G_CALLBACK (update_theme),
+                    NULL);
+
+  /* If gtk_settings_get_default() has worked, GdkDisplay
+   * exists, so we don't need to check that separately. */
+  g_signal_connect (gdk_display_get_default (),
+                    "setting-changed",
+                    G_CALLBACK (setting_changed_cb),
+                    NULL);
+
+  update_theme ();
 
   g_once_init_leave (&guard, 1);
 }
