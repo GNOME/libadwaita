a4c90bc6c2142b28e36c02caadd0266e5302e400
Adrien Plazas <kekun.plazas@laposte.net>
2021-04-02 14:57:04 +0200

    preferences-window: Make the search result margin adaptive
    This makes the top and bottom margins of the search results match the
    ones of preference pages, which depends of their clamp's width. This
    also avoids hardcoding the margins as properties.

--- src/adw-preferences-window.c
+++ src/adw-preferences-window.c
@@ -26,6 +26,11 @@
  * The #AdwPreferencesWindow widget presents an application's preferences
  * gathered into pages and groups. The preferences are searchable by the user.
  *
+ * # CSS nodes
+ *
+ * #AdwPreferencesWindow has a main CSS node with the name window and the style
+ * class .preferences.
+ *
  * Since: 1.0
  */
 
