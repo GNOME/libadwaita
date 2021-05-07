7fbcf684ce9c451b8158760a6e47c4444b06a590
Alexander Mikhaylenko <alexm@gnome.org>
2021-05-07 16:06:37 +0500

    doc: Drop the cross-reference hack
    It's not needed for gi-docgen.
    
    See https://gitlab.gnome.org/GNOME/libadwaita/-/merge_requests/104

--- doc.sh
+++ doc.sh
@@ -24,12 +24,3 @@ for REF in $REFS; do
 done
 
 # cp -r $DOC_DIR/$LATEST_STABLE_1 $DOC_DIR/1-latest
-
-find $DOC_DIR -type f -print0 | xargs -0 sed -i 's|\.\./gdk4/|https://developer.gnome.org/gdk4/stable/|g'
-find $DOC_DIR -type f -print0 | xargs -0 sed -i 's|\.\./gsk4/|https://developer.gnome.org/gsk4/stable/|g'
-find $DOC_DIR -type f -print0 | xargs -0 sed -i 's|\.\./gdk-pixbuf/|https://developer.gnome.org/gdk-pixbuf/stable/|g'
-find $DOC_DIR -type f -print0 | xargs -0 sed -i 's|\.\./gio/|https://developer.gnome.org/gio/stable/|g'
-find $DOC_DIR -type f -print0 | xargs -0 sed -i 's|\.\./glib/|https://developer.gnome.org/glib/stable/|g'
-find $DOC_DIR -type f -print0 | xargs -0 sed -i 's|\.\./gobject/|https://developer.gnome.org/gobject/stable/|g'
-find $DOC_DIR -type f -print0 | xargs -0 sed -i 's|\.\./gtk4/|https://developer.gnome.org/gtk4/stable/|g'
-find $DOC_DIR -type f -print0 | xargs -0 sed -i 's|\.\./pango/|https://developer.gnome.org/pango/stable/|g'
