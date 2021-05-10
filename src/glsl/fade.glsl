a1e8587d623791d3c992a934c5feb46ba209432a
Adrien Plazas <kekun.plazas@laposte.net>
2021-05-10 14:35:57 +0200

    glsl: Make the code style match C

--- src/glsl/fade.glsl
+++ src/glsl/fade.glsl
@@ -7,10 +7,12 @@ uniform sampler2D u_texture1;
 
 #define FADE_WIDTH 18
 
-void mainImage(out vec4 fragColor,
-               in vec2 fragCoord,
-               in vec2 resolution,
-               in vec2 uv) {
+void
+mainImage (out vec4 fragColor,
+           in vec2  fragCoord,
+           in vec2  resolution,
+           in vec2  uv)
+{
   float progress;
 
   fragColor = GskTexture (u_texture1, uv);
