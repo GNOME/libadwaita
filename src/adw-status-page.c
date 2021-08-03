8014b17994c1545fad7f5594f1dcdbf0bfc16293
Alexander Mikhaylenko <alexm@gnome.org>
2021-08-04 00:17:23 +0500

    status-page: Add .compact style class

--- src/adw-status-page.c
+++ src/adw-status-page.c
@@ -20,6 +20,9 @@
  *
  * `AdwStatusPage` has a main CSS node with name `statuspage`.
  *
+ * `AdwStatusPage` can use the `.compact` style class for when it needs to fit
+ * into a small space such a sidebar or a popover.
+ *
  * Since: 1.0
  */
 
