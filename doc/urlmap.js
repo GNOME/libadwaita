d698b07253f8db8902f31988c503697677e6d070
Alexander Mikhaylenko <alexm@gnome.org>
2021-09-20 19:55:54 +0500

    doc: Add glib and friends to urlmap
    Since they have gi-docgen docs now, we can link to them.

--- doc/urlmap.js
+++ doc/urlmap.js
@@ -5,6 +5,9 @@
 
 // A map between namespaces and base URLs for their online documentation
 baseURLs = [
+    [ 'GLib', 'https://docs.gtk.org/glib/' ],
+    [ 'Gio', 'https://docs.gtk.org/gio/' ],
+    [ 'GObject', 'https://docs.gtk.org/gobject/' ],
     [ 'Gdk', 'https://docs.gtk.org/gdk4/' ],
     [ 'Gsk', 'https://docs.gtk.org/gsk4/' ],
     [ 'Gtk', 'https://docs.gtk.org/gtk4/' ],
