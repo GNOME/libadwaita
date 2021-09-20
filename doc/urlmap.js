1842082d674066ac533b2869dcdbc964783eef38
Alexander Mikhaylenko <alexm@gnome.org>
2021-09-20 19:52:20 +0500

    doc: Drop modules we're not likely to use from urlmap
    They are unused and it's very unlikely we'll ever depend on them. For
    comparison, Pango and Gsk are unused as well, but we may very well start
    using them in future, so let's keep them.

--- doc/urlmap.js
+++ doc/urlmap.js
@@ -6,10 +6,7 @@
 // A map between namespaces and base URLs for their online documentation
 baseURLs = [
     [ 'Gdk', 'https://gnome.pages.gitlab.gnome.org/gtk/gdk4/' ],
-    [ 'GdkWayland', 'https://gnome.pages.gitlab.gnome.org/gtk/gdk4-wayland/' ],
-    [ 'GdkX11', 'https://gnome.pages.gitlab.gnome.org/gtk/gdk4-x11/' ],
     [ 'Gsk', 'https://gnome.pages.gitlab.gnome.org/gtk/gsk4/' ],
     [ 'Gtk', 'https://gnome.pages.gitlab.gnome.org/gtk/gtk4/' ],
     [ 'Pango', 'https://gnome.pages/gitlab.gnome.org/pango/pango/' ],
-    [ 'PangoCairo', 'https://gnome.pages.gitlab.gnome.org/pango/pangocairo/' ],
 ]
