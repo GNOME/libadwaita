GNOME/libadwaita

commit c2d77ba76d02bd5351db50e501ff048c4ad691ba

Remove g_auto* usage
It is unfortunately a GCCism, so use the traditional method instead.
Unfortunately the autocleanup compiler extensions are not standard across the
board.

src/adw-settings.c
@@ -100,11 +100,11 @@ read_portal_setting (AdwSettings  *self,
                      const char   *type,
                      GVariant    **out)
 {
-  g_autoptr (GError) error = NULL;
-  g_autoptr (GVariant) ret = NULL;
-  g_autoptr (GVariant) child = NULL;
-  g_autoptr (GVariant) child2 = NULL;
-  g_autoptr (GVariantType) out_type = NULL;
+  GError *error = NULL;
+  GVariant *ret;
+  GVariant *child, *child2;
+  GVariantType *out_type;
+  gboolean result = FALSE;
 
   ret = g_dbus_proxy_call_sync (self->settings_portal,
                                 "Read",
@@ -117,28 +117,19 @@ read_portal_setting (AdwSettings  *self,
     if (error->domain == G_DBUS_ERROR &&
         error->code == G_DBUS_ERROR_SERVICE_UNKNOWN) {
       g_debug ("Portal not found: %s", error->message);
-
-      return FALSE;
-    }
-
-    if (error->domain == G_DBUS_ERROR &&
-        error->code == G_DBUS_ERROR_UNKNOWN_METHOD) {
+    } else if (error->domain == G_DBUS_ERROR &&
+               error->code == G_DBUS_ERROR_UNKNOWN_METHOD) {
       g_debug ("Portal doesn't provide settings: %s", error->message);
-
-      return FALSE;
-    }
-
-    if (g_dbus_error_is_remote_error (error)) {
-      g_autofree char *remote_error = g_dbus_error_get_remote_error (error);
+    } else if (g_dbus_error_is_remote_error (error)) {
+      char *remote_error = g_dbus_error_get_remote_error (error);
 
       if (!g_strcmp0 (remote_error, PORTAL_ERROR_NOT_FOUND)) {
         g_debug ("Setting %s.%s of type %s not found", schema, name, type);
-
-        return FALSE;
       }
+      g_free (remote_error);
+    } else {
+      g_critical ("Couldn't read the %s setting: %s", name, error->message);
     }
-
-    g_critical ("Couldn't read the %s setting: %s", name, error->message);
 
     return FALSE;
   }
@@ -147,16 +138,23 @@ read_portal_setting (AdwSettings  *self,
   g_variant_get (child, "v", &child2);
 
   out_type = g_variant_type_new (type);
-  if (!g_variant_type_equal (g_variant_get_type (child2), out_type)) {
+  if (g_variant_type_equal (g_variant_get_type (child2), out_type)) {
+    *out = child2;
+
+    result = TRUE;
+  } else {
     g_critical ("Invalid type for %s.%s: expected %s, got %s",
                 schema, name, type, g_variant_get_type_string (child2));
 
-    return FALSE;
+    g_variant_unref (child2);
   }
 
-  *out = g_steal_pointer (&child2);
+  g_variant_type_free (out_type);
+  g_variant_unref (child);
+  g_variant_unref (ret);
+  g_clear_error (&error);
 
-  return TRUE;
+  return result;
 }
 
 static AdwSystemColorScheme
@@ -201,7 +199,7 @@ settings_portal_changed_cb (GDBusProxy  *proxy,
 {
   const char *namespace;
   const char *name;
-  g_autoptr (GVariant) value = NULL;
+  GVariant *value = NULL;
 
   if (g_strcmp0 (signal_name, "SettingChanged"))
     return;
@@ -213,6 +211,8 @@ settings_portal_changed_cb (GDBusProxy  *proxy,
       self->color_scheme_use_fdo_setting) {
     set_color_scheme (self, get_fdo_color_scheme (value));
 
+    g_variant_unref (value);
+
     return;
   }
 
@@ -221,12 +221,16 @@ settings_portal_changed_cb (GDBusProxy  *proxy,
       !self->color_scheme_use_fdo_setting) {
     set_color_scheme (self, get_gnome_color_scheme (value));
 
+    g_variant_unref (value);
+
     return;
   }
 
   if (!g_strcmp0 (namespace, "org.gnome.desktop.interface.a11y") &&
       !g_strcmp0 (name, "high-contrast")) {
     set_high_contrast (self, g_variant_get_boolean (value));
+
+    g_variant_unref (value);
 
     return;
   }
@@ -235,9 +239,8 @@ settings_portal_changed_cb (GDBusProxy  *proxy,
 static void
 init_portal (AdwSettings *self)
 {
-  g_autoptr (GError) error = NULL;
-  g_autoptr (GVariant) color_scheme_variant = NULL;
-  g_autoptr (GVariant) high_contrast_variant = NULL;
+  GError *error = NULL;
+  GVariant *variant;
 
   if (get_disable_portal ())
     return;
@@ -253,27 +256,35 @@ init_portal (AdwSettings *self)
   if (error) {
     g_debug ("Settings portal not found: %s", error->message);
 
+    g_error_free (error);
+
     return;
   }
 
   if (read_portal_setting (self, "org.freedesktop.appearance",
-                           "color-scheme", "u", &color_scheme_variant)) {
+                           "color-scheme", "u", &variant)) {
     self->has_color_scheme = TRUE;
     self->color_scheme_use_fdo_setting = TRUE;
-    self->color_scheme = get_fdo_color_scheme (color_scheme_variant);
+    self->color_scheme = get_fdo_color_scheme (variant);
+
+    g_variant_unref (variant);
   }
 
   if (!self->has_color_scheme &&
       read_portal_setting (self, "org.gnome.desktop.interface",
-                           "color-scheme", "s", &color_scheme_variant)) {
+                           "color-scheme", "s", &variant)) {
     self->has_color_scheme = TRUE;
-    self->color_scheme = get_gnome_color_scheme (color_scheme_variant);
+    self->color_scheme = get_gnome_color_scheme (variant);
+
+    g_variant_unref (variant);
   }
 
   if (read_portal_setting (self, "org.gnome.desktop.interface.a11y",
-                           "high-contrast", "b", &high_contrast_variant)) {
+                           "high-contrast", "b", &variant)) {
     self->has_high_contrast = TRUE;
-    self->high_contrast = g_variant_get_boolean (high_contrast_variant);
+    self->high_contrast = g_variant_get_boolean (variant);
+
+    g_variant_unref (variant);
   }
 
   if (!self->has_color_scheme && !self->has_high_contrast)
@@ -310,8 +321,7 @@ static void
 init_gsettings (AdwSettings *self)
 {
   GSettingsSchemaSource *source;
-  g_autoptr (GSettingsSchema) schema = NULL;
-  g_autoptr (GSettingsSchema) a11y_schema = NULL;
+  GSettingsSchema *schema;
 
 #ifndef G_OS_WIN32
   /* While we can access gsettings in flatpak, we can't do anything useful with
@@ -334,12 +344,14 @@ init_gsettings (AdwSettings *self)
                               "changed::color-scheme",
                               G_CALLBACK (gsettings_color_scheme_changed_cb),
                               self);
+
+    g_settings_schema_unref (schema);
   }
 
-  a11y_schema = g_settings_schema_source_lookup (source, "org.gnome.desktop.a11y.interface", TRUE);
-  if (a11y_schema &&
+  schema = g_settings_schema_source_lookup (source, "org.gnome.desktop.a11y.interface", TRUE);
+  if (schema &&
       !self->has_high_contrast &&
-      g_settings_schema_has_key (a11y_schema, "high-contrast")) {
+      g_settings_schema_has_key (schema, "high-contrast")) {
     self->has_high_contrast = TRUE;
     self->a11y_settings = g_settings_new ("org.gnome.desktop.a11y.interface");
     self->high_contrast = g_settings_get_boolean (self->a11y_settings, "high-contrast");
@@ -348,6 +360,8 @@ init_gsettings (AdwSettings *self)
                               "changed::high-contrast",
                               G_CALLBACK (gsettings_high_contrast_changed_cb),
                               self);
+
+    g_settings_schema_unref (schema);
   }
 }
 
@@ -356,7 +370,7 @@ init_gsettings (AdwSettings *self)
 static gboolean
 is_theme_high_contrast (GdkDisplay *display)
 {
-  g_auto (GValue) value = G_VALUE_INIT;
+  GValue value = G_VALUE_INIT;
   const char *theme_name;
 
   g_value_init (&value, G_TYPE_STRING);
@@ -364,6 +378,8 @@ is_theme_high_contrast (GdkDisplay *display)
     return FALSE;
 
   theme_name = g_value_get_string (&value);
+
+  g_value_unset (&value);
 
   return !g_strcmp0 (theme_name, "HighContrast") ||
          !g_strcmp0 (theme_name, "HighContrastInverse");

demo/adwaita-demo.c
@@ -49,7 +49,7 @@ show_about (GSimpleAction *action,
 
   GtkApplication *app = GTK_APPLICATION (user_data);
   GtkWindow *window = gtk_application_get_active_window (app);
-  g_autofree char *version = NULL;
+  char *version;
 
   version = g_strdup_printf ("%s\nRunning against libadwaita %d.%d.%d, GTK %d.%d.%d",
                              ADW_VERSION_S,
@@ -73,6 +73,7 @@ show_about (GSimpleAction *action,
                          "artists", artists,
                          "translator-credits", _("translator-credits"),
                          NULL);
+  g_free (version);
 }
 
 static void

demo/pages/animations/adw-demo-page-animations.c
@@ -242,12 +242,14 @@ timed_animation_cb (double     value,
 static void
 notify_spring_params_change (AdwDemoPageAnimations *self)
 {
-  g_autoptr (AdwSpringParams) spring_params =
+  AdwSpringParams *spring_params =
     adw_spring_params_new_full (gtk_spin_button_get_value (self->spring_animation_damping),
                                 gtk_spin_button_get_value (self->spring_animation_mass),
                                 gtk_spin_button_get_value (self->spring_animation_stiffness));
 
   adw_spring_animation_set_spring_params (ADW_SPRING_ANIMATION (self->spring_animation), spring_params);
+
+  adw_spring_params_unref (spring_params);
 }
 
 static void

demo/pages/avatar/adw-demo-page-avatar.c
@@ -68,7 +68,7 @@ static void
 populate_contacts (AdwDemoPageAvatar *self)
 {
   for (int i = 0; i < 30; i++) {
-    g_autofree char *name = create_random_name ();
+    char *name = create_random_name ();
     GtkWidget *contact = adw_action_row_new ();
     GtkWidget *avatar = adw_avatar_new (40, name, TRUE);
 
@@ -78,6 +78,8 @@ populate_contacts (AdwDemoPageAvatar *self)
     adw_preferences_row_set_title (ADW_PREFERENCES_ROW (contact), name);
     adw_action_row_add_prefix (ADW_ACTION_ROW (contact), avatar);
     gtk_list_box_append (self->contacts, contact);
+
+    g_free (name);
   }
 }
 
@@ -87,10 +89,10 @@ open_response_cb (AdwDemoPageAvatar *self,
                   GtkFileChooser    *chooser)
 {
   if (response == GTK_RESPONSE_ACCEPT) {
-    g_autoptr (GFile) file = gtk_file_chooser_get_file (chooser);
-    g_autoptr (GFileInfo) info = NULL;
-    g_autoptr (GdkTexture) texture = NULL;
-    g_autoptr (GError) error = NULL;
+    GFile *file = gtk_file_chooser_get_file (chooser);
+    GFileInfo *info;
+    GdkTexture *texture;
+    GError *error = NULL;
 
     info = g_file_query_info (file,
                               G_FILE_ATTRIBUTE_STANDARD_DISPLAY_NAME,
@@ -109,6 +111,11 @@ open_response_cb (AdwDemoPageAvatar *self,
       g_critical ("Failed to create texture from file: %s", error->message);
 
     adw_avatar_set_custom_image (self->avatar, texture ? GDK_PAINTABLE (texture) : NULL);
+
+    g_clear_error (&error);
+    g_clear_object (&info);
+    g_object_unref (texture);
+    g_object_unref (file);
   }
 
   gtk_native_dialog_destroy (GTK_NATIVE_DIALOG (chooser));
@@ -145,12 +152,15 @@ save_response_cb (AdwDemoPageAvatar *self,
                   GtkFileChooser    *chooser)
 {
   if (response == GTK_RESPONSE_ACCEPT) {
-    g_autoptr (GFile) file = gtk_file_chooser_get_file (chooser);
-    g_autoptr (GdkTexture) texture =
+    GFile *file = gtk_file_chooser_get_file (chooser);
+    GdkTexture *texture =
       adw_avatar_draw_to_texture (self->avatar,
                                   gtk_widget_get_scale_factor (GTK_WIDGET (self)));
 
     gdk_texture_save_to_png (texture, g_file_peek_path (file));
+
+    g_object_unref (texture);
+    g_object_unref (file);
   }
 
   gtk_native_dialog_destroy (GTK_NATIVE_DIALOG (chooser));
@@ -192,7 +202,7 @@ adw_demo_page_avatar_class_init (AdwDemoPageAvatarClass *klass)
 static void
 adw_demo_page_avatar_init (AdwDemoPageAvatar *self)
 {
-  g_autofree char *name = NULL;
+  char *name;
 
   gtk_widget_init_template (GTK_WIDGET (self));
 
@@ -201,4 +211,6 @@ adw_demo_page_avatar_init (AdwDemoPageAvatar *self)
 
   populate_contacts (self);
   avatar_remove_cb (self);
+
+  g_free (name);
 }

demo/pages/tab-view/adw-tab-view-demo-window.c
@@ -106,7 +106,7 @@ tab_new (GSimpleAction *action,
          gpointer       user_data)
 {
   AdwTabViewDemoWindow *self = ADW_TAB_VIEW_DEMO_WINDOW (user_data);
-  g_autofree char *title = NULL;
+  char *title;
   AdwTabPage *page;
   GtkWidget *content;
   GIcon *icon;
@@ -123,6 +123,8 @@ tab_new (GSimpleAction *action,
   gtk_widget_grab_focus (content);
 
   next_page++;
+
+  g_free (title);
 }
 
 static AdwTabPage *
@@ -256,13 +258,15 @@ tab_change_indicator (GSimpleAction *action,
 {
   AdwTabViewDemoWindow *self = ADW_TAB_VIEW_DEMO_WINDOW (user_data);
   gboolean indicator = g_variant_get_boolean (parameter);
-  g_autoptr (GIcon) icon = NULL;
+  GIcon *icon = NULL;
 
   if (indicator)
     icon = get_indicator_icon (get_current_page (self));
 
   adw_tab_page_set_indicator_icon (get_current_page (self), icon);
   g_simple_action_set_state (action, g_variant_new_boolean (indicator));
+
+  g_clear_object (&icon);
 }
 
 static void
@@ -274,9 +278,11 @@ tab_change_icon (GSimpleAction *action,
   gboolean enable_icon = g_variant_get_boolean (parameter);
 
   if (enable_icon) {
-    g_autoptr (GIcon) icon = get_random_icon (self);
+    GIcon *icon = get_random_icon (self);
 
     adw_tab_page_set_icon (get_current_page (self), icon);
+
+    g_object_unref (icon);
   } else {
     adw_tab_page_set_icon (get_current_page (self), NULL);
   }
@@ -290,9 +296,11 @@ tab_refresh_icon (GSimpleAction *action,
                   gpointer       user_data)
 {
   AdwTabViewDemoWindow *self = ADW_TAB_VIEW_DEMO_WINDOW (user_data);
-  g_autoptr (GIcon) icon = get_random_icon (self);
+  GIcon *icon = get_random_icon (self);
 
   adw_tab_page_set_icon (get_current_page (self), icon);
+
+  g_object_unref (icon);
 }
 
 static void
@@ -436,7 +444,7 @@ static void
 indicator_activated_cb (AdwTabViewDemoWindow *self,
                         AdwTabPage           *page)
 {
-  g_autoptr (GIcon) icon = NULL;
+  GIcon *icon;
   gboolean muted;
 
   muted = GPOINTER_TO_INT (g_object_get_data (G_OBJECT (page),
@@ -449,6 +457,8 @@ indicator_activated_cb (AdwTabViewDemoWindow *self,
   icon = get_indicator_icon (page);
 
   adw_tab_page_set_indicator_icon (page, icon);
+
+  g_object_unref (icon);
 }
 
 static gboolean

demo/pages/toasts/adw-demo-page-toasts.c
@@ -44,11 +44,18 @@ toast_add_cb (AdwDemoPageToasts *self)
 static void
 toast_add_with_button_cb (AdwDemoPageToasts *self)
 {
-  g_autofree char *title = NULL;
+  char *title;
 
   self->toast_undo_items++;
 
-  if (!self->undo_toast) {
+  if (self->undo_toast) {
+    title =
+      g_strdup_printf (ngettext ("<span font_features='tnum=1'>%d</span> item deleted",
+                                 "<span font_features='tnum=1'>%d</span> items deleted",
+                                 self->toast_undo_items), self->toast_undo_items);
+
+    adw_toast_set_title (self->undo_toast, title);
+  } else {
     title = g_strdup_printf (_("‘%s’ deleted"), "Lorem Ipsum");
 
     self->undo_toast = adw_toast_new (title);
@@ -62,16 +69,9 @@ toast_add_with_button_cb (AdwDemoPageToasts *self)
     add_toast (self, self->undo_toast);
 
     gtk_widget_action_set_enabled (GTK_WIDGET (self), "toast.dismiss", TRUE);
-
-    return;
   }
 
-  title =
-    g_strdup_printf (ngettext ("<span font_features='tnum=1'>%d</span> item deleted",
-                               "<span font_features='tnum=1'>%d</span> items deleted",
-                               self->toast_undo_items), self->toast_undo_items);
-
-  adw_toast_set_title (self->undo_toast, title);
+  g_free (title);
 }
 
 static void
@@ -124,7 +124,7 @@ adw_demo_page_toasts_init (AdwDemoPageToasts *self)
 void
 adw_demo_page_toasts_undo (AdwDemoPageToasts *self)
 {
-  g_autofree char *title =
+  char *title =
     g_strdup_printf (ngettext ("Undoing deleting <span font_features='tnum=1'>%d</span> item…",
                                "Undoing deleting <span font_features='tnum=1'>%d</span> items…",
                                self->toast_undo_items), self->toast_undo_items);
@@ -133,4 +133,6 @@ adw_demo_page_toasts_undo (AdwDemoPageToasts *self)
   adw_toast_set_priority (toast, ADW_TOAST_PRIORITY_HIGH);
 
   add_toast (self, toast);
+
+  g_free (title);
 }

src/adw-application.c
@@ -111,10 +111,9 @@ static void
 init_providers (AdwApplication *self)
 {
   AdwApplicationPrivate *priv = adw_application_get_instance_private (self);
-
-  const char *base_path = NULL;
-  g_autofree char *base_uri = NULL;
-  g_autoptr (GFile) base_file = NULL;
+  const char *base_path;
+  char *base_uri;
+  GFile *base_file;
 
   base_path = g_application_get_resource_base_path (G_APPLICATION (self));
 
@@ -132,6 +131,9 @@ init_providers (AdwApplication *self)
                            g_file_get_child (base_file, "style-hc.css"));
   init_provider_from_file (&priv->hc_dark_style_provider,
                            g_file_get_child (base_file, "style-hc-dark.css"));
+
+  g_object_unref (base_file);
+  g_free (base_uri);
 }
 
 static void

src/adw-avatar.c
@@ -83,10 +83,12 @@ static char *
 extract_initials_from_text (const char *text)
 {
   GString *initials;
-  g_autofree char *p = g_utf8_strup (text, -1);
-  g_autofree char *normalized = g_utf8_normalize (g_strstrip (p), -1, G_NORMALIZE_DEFAULT_COMPOSE);
+  char *p = g_utf8_strup (text, -1);
+  char *normalized = g_utf8_normalize (g_strstrip (p), -1, G_NORMALIZE_DEFAULT_COMPOSE);
   gunichar unichar;
   char *q = NULL;
+
+  g_clear_pointer (&p, g_free);
 
   if (normalized == NULL)
     return NULL;
@@ -103,6 +105,8 @@ extract_initials_from_text (const char *text)
     unichar = g_utf8_get_char (q);
     g_string_append_unichar (initials, unichar);
   }
+
+  g_free (normalized);
 
   return g_string_free (initials, FALSE);
 }
@@ -121,29 +125,33 @@ update_visibility (AdwAvatar *self)
 static void
 set_class_color (AdwAvatar *self)
 {
-  g_autofree GRand *rand = NULL;
-  g_autofree char *new_class = NULL;
-  g_autofree char *old_class = g_strdup_printf ("color%d", self->color_class);
+  char *old_class, *new_class;
 
+  old_class = g_strdup_printf ("color%d", self->color_class);
   gtk_widget_remove_css_class (self->gizmo, old_class);
 
   if (self->text == NULL || strlen (self->text) == 0) {
     /* Use a random color if we don't have a text */
-    rand = g_rand_new ();
+    GRand *rand = g_rand_new ();
+
     self->color_class = g_rand_int_range (rand, 1, NUMBER_OF_COLORS);
+
+    g_rand_free (rand);
   } else {
     self->color_class = (g_str_hash (self->text) % NUMBER_OF_COLORS) + 1;
   }
 
   new_class = g_strdup_printf ("color%d", self->color_class);
-
   gtk_widget_add_css_class (self->gizmo, new_class);
+
+  g_free (old_class);
+  g_free (new_class);
 }
 
 static void
 update_initials (AdwAvatar *self)
 {
-  g_autofree char *initials = NULL;
+  char *initials;
 
   if (gtk_image_get_paintable (self->custom_image) != NULL ||
       !self->show_initials ||
@@ -154,6 +162,8 @@ update_initials (AdwAvatar *self)
   initials = extract_initials_from_text (self->text);
 
   gtk_label_set_label (self->label, initials);
+
+  g_free (initials);
 }
 
 static void
@@ -633,7 +643,7 @@ adw_avatar_set_custom_image (AdwAvatar    *self,
       gtk_image_set_from_paintable (self->custom_image, custom_image);
     } else {
       GtkSnapshot *snapshot = gtk_snapshot_new ();
-      g_autoptr (GdkPaintable) square_image = NULL;
+      GdkPaintable *square_image;
       int size = MIN (width, height);
 
       gtk_snapshot_translate (snapshot, &GRAPHENE_POINT_INIT ((size - width) / 2.f, (size - height) / 2.f));
@@ -641,6 +651,8 @@ adw_avatar_set_custom_image (AdwAvatar    *self,
 
       square_image = gtk_snapshot_free_to_paintable (snapshot, &GRAPHENE_SIZE_INIT (size, size));
       gtk_image_set_from_paintable (self->custom_image, square_image);
+
+      g_object_unref (square_image);
     }
 
     gtk_widget_add_css_class (self->gizmo, "image");
@@ -724,7 +736,8 @@ GdkTexture *
 adw_avatar_draw_to_texture (AdwAvatar *self,
                             int        scale_factor)
 {
-  g_autoptr (GskRenderNode) node = NULL;
+  GdkTexture *result;
+  GskRenderNode *node;
   GtkSnapshot *snapshot;
   GtkNative *native;
   GskRenderer *renderer;
@@ -744,5 +757,9 @@ adw_avatar_draw_to_texture (AdwAvatar *self,
   native = gtk_widget_get_native (GTK_WIDGET (self));
   renderer = gtk_native_get_renderer (native);
 
-  return gsk_renderer_render_texture (renderer, node, &GRAPHENE_RECT_INIT (0, 0, size, size));
+  result = gsk_renderer_render_texture (renderer, node, &GRAPHENE_RECT_INIT (0, 0, size, size));
+
+  gsk_render_node_unref (node);
+
+  return result;
 }

commit f35d9b38b639857731414c0a3d70904e9926cdfe

settings: Fix legacy high contrast handling
It regressed after c2d77ba76d02bd5351db50e501ff048c4ad691ba

src/adw-settings.c
@@ -372,6 +372,7 @@ is_theme_high_contrast (GdkDisplay *display)
 {
   GValue value = G_VALUE_INIT;
   const char *theme_name;
+  gboolean ret;
 
   g_value_init (&value, G_TYPE_STRING);
   if (!gdk_display_get_setting (display, "gtk-theme-name", &value))
@@ -379,10 +380,12 @@ is_theme_high_contrast (GdkDisplay *display)
 
   theme_name = g_value_get_string (&value);
 
+  ret = !g_strcmp0 (theme_name, "HighContrast") ||
+        !g_strcmp0 (theme_name, "HighContrastInverse");
+
   g_value_unset (&value);
 
-  return !g_strcmp0 (theme_name, "HighContrast") ||
-         !g_strcmp0 (theme_name, "HighContrastInverse");
+  return ret;
 }
 
 static void