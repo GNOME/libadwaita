1511f5a4ccb24285a60289e0c24cbc7de2e79bae
Emmanuele Bassi <ebassi@gnome.org>
2022-02-28 17:19:43 +0000

    toast: Fix indentation in the header

--- src/adw-toast.h
+++ src/adw-toast.h
@@ -46,7 +46,7 @@ void        adw_toast_set_button_label (AdwToast   *self,
                                         const char *button_label);
 
 ADW_AVAILABLE_IN_ALL
-const char *adw_toast_get_action_name (AdwToast *self);
+const char *adw_toast_get_action_name (AdwToast   *self);
 ADW_AVAILABLE_IN_ALL
 void        adw_toast_set_action_name (AdwToast   *self,
                                        const char *action_name);
