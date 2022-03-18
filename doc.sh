fab8a1687cade17cb743eba040728591b355d630
Alexander Mikhaylenko <alexm@gnome.org>
2022-03-18 23:43:22 +0400

    Fix docs
    - Make the dir names actually match the ones in html
    - Add redirects for the old ones

--- doc.sh
+++ doc.sh
@@ -15,12 +15,21 @@ IFS='
 mkdir -p $DOC_DIR
 
 for REF in $REFS; do
+  API_VERSION=`echo $REF | sed 's/libadwaita-\([0-9][0-9]*\)-\([0-9][0-9]*\)/\1.\2/'`
+
   curl -L --output "$REF.zip" "https://gitlab.gnome.org/GNOME/libadwaita/-/jobs/artifacts/$REF/download?job=doc"
   unzip -d "$REF" "$REF.zip"
-  mv "$REF/_doc" $DOC_DIR/$REF
+  mv "$REF/_doc" $DOC_DIR/$API_VERSION
 
   rm "$REF.zip"
   rm -rf "$REF"
 done
 
 ln -s $LATEST_STABLE_1 $DOC_DIR/1-latest
+
+# Redirect the old links
+ln -s $LATEST_STABLE_1 $DOC_DIR/1.0.0-alpha.1
+ln -s $LATEST_STABLE_1 $DOC_DIR/1.0.0-alpha.2
+ln -s $LATEST_STABLE_1 $DOC_DIR/1.0.0.alpha.3
+ln -s $LATEST_STABLE_1 $DOC_DIR/1.0.0.beta.1
+ln -s $LATEST_STABLE_1 $DOC_DIR/1.0.0
