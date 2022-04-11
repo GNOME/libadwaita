GNOME/libadwaita

commit cefc3ec8cf327c6a9959f1c3ca0929559398e348

tab-box: Reliably finish scroll animation
Prevent it from being stuck with 1px left.

src/adw-tab-box.c
@@ -3186,7 +3186,7 @@ adw_tab_box_size_allocate (GtkWidget *widget,
     self->scheduled_scroll.info = NULL;
   }
 
-  if (adw_animation_get_state (self->scroll_animation) == ADW_ANIMATION_PLAYING) {
+  if (adw_animation_get_state (self->scroll_animation) != ADW_ANIMATION_IDLE) {
     self->block_scrolling = TRUE;
     gtk_adjustment_set_value (self->adjustment,
                               get_scroll_animation_value (self));

commit fe1445c716cb7a29928144218a9b98fbe0169b62

tab-box: Fix autoscroll for dragged tab
We call adw_animation_pause() in that case and so that check was failing.

Regressed in cefc3ec8cf327c6a9959f1c3ca0929559398e348

src/adw-tab-box.c
@@ -3060,6 +3060,7 @@ adw_tab_box_size_allocate (GtkWidget *widget,
                            int        baseline)
 {
   AdwTabBox *self = ADW_TAB_BOX (widget);
+  AdwAnimationState state;
   gboolean is_rtl;
   GList *l;
   GtkAllocation child_allocation;
@@ -3186,7 +3187,8 @@ adw_tab_box_size_allocate (GtkWidget *widget,
     self->scheduled_scroll.info = NULL;
   }
 
-  if (adw_animation_get_state (self->scroll_animation) != ADW_ANIMATION_IDLE) {
+  state = adw_animation_get_state (self->scroll_animation);
+  if (state == ADW_ANIMATION_PLAYING || state == ADW_ANIMATION_FINISHED) {
     self->block_scrolling = TRUE;
     gtk_adjustment_set_value (self->adjustment,
                               get_scroll_animation_value (self));