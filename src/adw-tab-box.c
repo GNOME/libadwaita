0403b0378b66ed0d245e202d90280c4cbf132988
Alexander Mikhaylenko <alexm@gnome.org>
2022-05-01 21:58:50 +0400

    tab-box: Claim the sequence on press for middle click
    Make it work with window handles.
    
    Fixes https://gitlab.gnome.org/GNOME/libadwaita/-/issues/471
    
    
    (cherry picked from commit 50db2aa22d3dfc1afc2b3f6e90758305865c8b20)
    
    (cherry picked from commit 1c8f3ffa08c6da26ff25aeb18b615a3f65c35b76)

--- src/adw-tab-box.c
+++ src/adw-tab-box.c
@@ -2781,8 +2781,11 @@ pressed_cb (AdwTabBox  *self,
 
   button = gtk_gesture_single_get_current_button (GTK_GESTURE_SINGLE (gesture));
 
-  if (button == GDK_BUTTON_MIDDLE)
+  if (button == GDK_BUTTON_MIDDLE) {
+    gtk_gesture_set_state (gesture, GTK_EVENT_SEQUENCE_CLAIMED);
+
     return;
+  }
 
   if (button != GDK_BUTTON_PRIMARY) {
     gtk_gesture_set_state (gesture, GTK_EVENT_SEQUENCE_DENIED);
@@ -2822,7 +2825,6 @@ released_cb (AdwTabBox  *self,
   button = gtk_gesture_single_get_current_button (GTK_GESTURE_SINGLE (gesture));
 
   if (button == GDK_BUTTON_MIDDLE) {
-    gtk_gesture_set_state (gesture, GTK_EVENT_SEQUENCE_CLAIMED);
     adw_tab_view_close_page (self->view, info->page);
 
     return;
