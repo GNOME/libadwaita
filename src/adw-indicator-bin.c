d597cd898378e6ecb73345e836f2efa6951e02dd
Maximiliano Sandoval R <msandova@gnome.org>
2023-03-30 22:47:19 +0200

    indicator-bin: Check if child has parent in setter

--- src/adw-indicator-bin.c
+++ src/adw-indicator-bin.c
@@ -342,6 +342,9 @@ adw_indicator_bin_set_child (AdwIndicatorBin *self,
   g_return_if_fail (ADW_IS_INDICATOR_BIN (self));
   g_return_if_fail (child == NULL || GTK_IS_WIDGET (child));
 
+  if (child)
+    g_return_if_fail (gtk_widget_get_parent (child) == NULL);
+
   if (self->child == child)
     return;
 
