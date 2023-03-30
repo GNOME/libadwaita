1c8b92cd6c3c1458faa4c44ff1ca1e07e7514fba
Maximiliano Sandoval R <msandova@gnome.org>
2023-03-30 21:09:47 +0200

    clamp: Check child has parent in setter

--- src/adw-clamp.c
+++ src/adw-clamp.c
@@ -286,6 +286,9 @@ adw_clamp_set_child (AdwClamp  *self,
   g_return_if_fail (ADW_IS_CLAMP (self));
   g_return_if_fail (child == NULL || GTK_IS_WIDGET (child));
 
+  if (child)
+    g_return_if_fail (gtk_widget_get_parent (child) == NULL);
+
   if (self->child == child)
     return;
 
