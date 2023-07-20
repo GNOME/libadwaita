GNOME/libadwaita

commit 720f701d52c12f915401d7039bd5faf39873dc95

Use g_(timeout|idle)_add_once()

doc/tools/screenshot.c
@@ -227,7 +227,7 @@ load_css (const char *name)
   return provider;
 }
 
-static gboolean
+static void
 take_screenshot_cb (ScreenshotData *data)
 {
   if (GTK_IS_POPOVER (data->widget))
@@ -289,8 +289,6 @@ take_screenshot_cb (ScreenshotData *data)
                             G_CALLBACK (draw_paintable), data);
 
   gtk_widget_queue_draw (data->widget);
-
-  return G_SOURCE_REMOVE;
 }
 
 static void
@@ -395,7 +393,7 @@ take_screenshot (const char *name,
 
   if (!wait) {
     if (hscroll_widget || vscroll_widget)
-      g_idle_add (G_SOURCE_FUNC (take_screenshot_cb), data);
+      g_idle_add_once ((GSourceOnceFunc) take_screenshot_cb, data);
     else
       take_screenshot_cb (data);
   }

src/adw-back-button.c
@@ -305,7 +305,7 @@ pop_to_page_cb (AdwBackButton *self,
   g_slist_free_full (pop_data.pop_before, g_free);
 }
 
-static gboolean
+static void
 clear_menu (AdwBackButton *self)
 {
   g_clear_pointer (&self->navigation_menu, gtk_widget_unparent);
@@ -316,8 +316,6 @@ clear_menu (AdwBackButton *self)
   }
 
   self->clear_menu_id = 0;
-
-  return G_SOURCE_REMOVE;
 }
 
 static void
@@ -327,7 +325,7 @@ navigation_menu_closed_cb (AdwBackButton *self)
 
   gtk_widget_unset_state_flags (button, GTK_STATE_FLAG_CHECKED);
 
-  self->clear_menu_id = g_idle_add (G_SOURCE_FUNC (clear_menu), self);
+  self->clear_menu_id = g_idle_add_once ((GSourceOnceFunc) clear_menu, self);
 }
 
 static void

src/adw-carousel.c
@@ -465,11 +465,10 @@ update_orientation (AdwCarousel *self)
   set_orientable_style_classes (GTK_ORIENTABLE (self));
 }
 
-static gboolean
+static void
 scroll_timeout_cb (AdwCarousel *self)
 {
   self->can_scroll = TRUE;
-  return G_SOURCE_REMOVE;
 }
 
 static gboolean
@@ -535,7 +534,9 @@ scroll_cb (AdwCarousel              *self,
 
   self->can_scroll = FALSE;
   self->scroll_timeout_id =
-   g_timeout_add (SCROLL_TIMEOUT_DURATION, (GSourceFunc) scroll_timeout_cb, self);
+   g_timeout_add_once (SCROLL_TIMEOUT_DURATION,
+                       (GSourceOnceFunc) scroll_timeout_cb,
+                       self);
 
   return GDK_EVENT_STOP;
 }
@@ -1388,7 +1389,7 @@ typedef struct {
   gboolean animate;
 } ScrollData;
 
-static gboolean
+static void
 scroll_to_idle_cb (ScrollData *data)
 {
   do_scroll_to (data->carousel, data->widget, data->animate);
@@ -1396,8 +1397,6 @@ scroll_to_idle_cb (ScrollData *data)
   g_object_unref (data->carousel);
   g_object_unref (data->widget);
   g_free (data);
-
-  return FALSE;
 }
 
 /**
@@ -1432,7 +1431,7 @@ adw_carousel_scroll_to (AdwCarousel *self,
     data->widget = g_object_ref (widget);
     data->animate = animate;
 
-    g_idle_add (G_SOURCE_FUNC (scroll_to_idle_cb), data);
+    g_idle_add_once ((GSourceOnceFunc) scroll_to_idle_cb, data);
     return;
   }
 

src/adw-message-dialog.c
@@ -268,7 +268,7 @@ parent_size_cb (AdwMessageDialog *self)
   gtk_widget_queue_resize (GTK_WIDGET (self));
 }
 
-static gboolean
+static void
 parent_state_idle_cb (AdwMessageDialog *self)
 {
   AdwMessageDialogPrivate *priv = adw_message_dialog_get_instance_private (self);
@@ -276,8 +276,6 @@ parent_state_idle_cb (AdwMessageDialog *self)
   parent_size_cb (self);
 
   priv->parent_state_idle_id = 0;
-
-  return G_SOURCE_REMOVE;
 }
 
 static void
@@ -287,7 +285,8 @@ parent_state_cb (AdwMessageDialog *self)
 
   g_clear_handle_id (&priv->parent_state_idle_id, g_source_remove);
 
-  priv->parent_state_idle_id = g_idle_add (G_SOURCE_FUNC (parent_state_idle_cb), self);
+  priv->parent_state_idle_id =
+    g_idle_add_once ((GSourceOnceFunc) parent_state_idle_cb, self);
 }
 
 static void

src/adw-navigation-split-view.c
@@ -353,12 +353,10 @@ update_navigation_stack (AdwNavigationSplitView *self)
                                stack, i);
 }
 
-static gboolean
+static void
 changing_page_done_cb (AdwNavigationSplitView *self)
 {
   self->changing_page = FALSE;
-
-  return G_SOURCE_REMOVE;
 }
 
 static gboolean
@@ -1223,7 +1221,7 @@ adw_navigation_split_view_set_show_content (AdwNavigationSplitView *self,
   }
 
   self->changing_page = TRUE;
-  g_idle_add (G_SOURCE_FUNC (changing_page_done_cb), self);
+  g_idle_add_once ((GSourceOnceFunc) changing_page_done_cb, self);
 
   if (show_content)
     adw_navigation_view_push (ADW_NAVIGATION_VIEW (self->navigation_view),

src/adw-settings-impl-win32.c
@@ -174,12 +174,11 @@ color_values_changed (AdwSettingsImplWin32 *self)
   return S_OK;
 }
 
-static int
+static void
 color_values_changed_idle (gpointer settings)
 {
   color_values_changed (settings);
   g_object_unref (settings);
-  return G_SOURCE_REMOVE;
 }
 
 static HRESULT STDMETHODCALLTYPE
@@ -193,7 +192,7 @@ ColorValuesChanged_Invoke (__FITypedEventHandler_2_Windows__CUI__CViewManagement
   settings = g_weak_ref_get (&handler->settings);
   if (settings != NULL) {
     /* Event handler is invoked from another thread */
-    g_idle_add (color_values_changed_idle, settings);
+    g_idle_add_once (color_values_changed_idle, settings);
   }
   return S_OK;
 }

src/adw-style-manager.c
@@ -117,15 +117,13 @@ register_display (GdkDisplayManager *display_manager,
                     NULL);
 }
 
-static gboolean
+static void
 enable_animations_cb (AdwStyleManager *self)
 {
   gtk_style_context_remove_provider_for_display (self->display,
                                                  GTK_STYLE_PROVIDER (self->animations_provider));
 
   self->animation_timeout_id = 0;
-
-  return G_SOURCE_REMOVE;
 }
 
 static void
@@ -172,9 +170,9 @@ update_stylesheet (AdwStyleManager *self)
   }
 
   self->animation_timeout_id =
-    g_timeout_add (SWITCH_DURATION,
-                   G_SOURCE_FUNC (enable_animations_cb),
-                   self);
+    g_timeout_add_once (SWITCH_DURATION,
+                        (GSourceOnceFunc) enable_animations_cb,
+                        self);
 }
 
 static gboolean

src/adw-tab-box.c
@@ -834,14 +834,12 @@ get_scroll_animation_value (AdwTabBox *self)
   return round (adw_lerp (self->scroll_animation_from, to, value));
 }
 
-static gboolean
+static void
 drop_switch_timeout_cb (AdwTabBox *self)
 {
   self->drop_switch_timeout_id = 0;
   adw_tab_view_set_selected_page (self->view,
                                   self->drop_target_tab->page);
-
-  return G_SOURCE_REMOVE;
 }
 
 static void
@@ -858,9 +856,9 @@ set_drop_target_tab (AdwTabBox *self,
 
   if (self->drop_target_tab) {
     self->drop_switch_timeout_id =
-      g_timeout_add (DROP_SWITCH_TIMEOUT,
-                     (GSourceFunc) drop_switch_timeout_cb,
-                     self);
+      g_timeout_add_once (DROP_SWITCH_TIMEOUT,
+                          (GSourceOnceFunc) drop_switch_timeout_cb,
+                          self);
   }
 }
 
@@ -2300,13 +2298,11 @@ remove_animation_done_cb (TabInfo *info)
   update_separators (self);
 }
 
-static gboolean
+static void
 remove_placeholder_scroll_cb (AdwTabBox *self)
 {
   animate_scroll_relative (self, -self->placeholder_scroll_offset, CLOSE_ANIMATION_DURATION);
   self->placeholder_scroll_offset = 0;
-
-  return G_SOURCE_REMOVE;
 }
 
 static void
@@ -2324,7 +2320,7 @@ remove_placeholder (AdwTabBox *self)
   if (info->appear_animation)
     adw_animation_skip (info->appear_animation);
 
-  g_idle_add ((GSourceFunc) remove_placeholder_scroll_cb, self);
+  g_idle_add_once ((GSourceOnceFunc) remove_placeholder_scroll_cb, self);
 
   target = adw_callback_animation_target_new ((AdwAnimationTargetFunc)
                                               appear_animation_value_cb,
@@ -2739,13 +2735,11 @@ view_drag_drop_cb (AdwTabBox      *self,
 
 /* DND autoscrolling */
 
-static gboolean
+static void
 reset_drop_target_tab_cb (AdwTabBox *self)
 {
   self->reset_drop_target_tab_id = 0;
   set_drop_target_tab (self, NULL);
-
-  return G_SOURCE_REMOVE;
 }
 
 static void
@@ -2763,7 +2757,7 @@ drag_leave_cb (AdwTabBox               *self,
 
   if (!self->reset_drop_target_tab_id)
     self->reset_drop_target_tab_id =
-      g_idle_add ((GSourceFunc) reset_drop_target_tab_cb, self);
+      g_idle_add_once ((GSourceOnceFunc) reset_drop_target_tab_cb, self);
 
   end_autoscroll (self);
 }
@@ -2802,12 +2796,10 @@ drag_enter_motion_cb (AdwTabBox               *self,
 
 /* Context menu */
 
-static gboolean
+static void
 reset_setup_menu_cb (AdwTabBox *self)
 {
   g_signal_emit_by_name (self->view, "setup-menu", NULL);
-
-  return G_SOURCE_REMOVE;
 }
 
 static void
@@ -2819,7 +2811,7 @@ touch_menu_notify_visible_cb (AdwTabBox *self)
   self->hovering = FALSE;
   update_hover (self);
 
-  g_idle_add ((GSourceFunc) reset_setup_menu_cb, self);
+  g_idle_add_once ((GSourceOnceFunc) reset_setup_menu_cb, self);
 }
 
 static void

commit f8aecbf1347ad410d387a853cdfe8c995798c1d4

screenshot: Handle timeouts properly
Fix a fallout from 720f701d52c12f915401d7039bd5faf39873dc95

doc/tools/screenshot.c
@@ -159,7 +159,7 @@ crop_alpha (GdkPixbuf *pixbuf)
   return gdk_pixbuf_new_subpixbuf (pixbuf, left, top, right - left, bottom - top);
 }
 
-static gboolean
+static void
 draw_paintable_cb (ScreenshotData *data)
 {
   GtkSnapshot *snapshot;
@@ -227,8 +227,6 @@ draw_paintable_cb (ScreenshotData *data)
 
   g_object_unref (texture);
   gsk_render_node_unref (node);
-
-  return G_SOURCE_REMOVE;
 }
 
 static void
@@ -239,7 +237,7 @@ draw_paintable (ScreenshotData *data)
                                         data);
 
   /* Handle the case where something immediately invalidates allocation. */
-  g_timeout_add (50, G_SOURCE_FUNC (draw_paintable_cb), data);
+  g_timeout_add_once (50, (GSourceOnceFunc) draw_paintable_cb, data);
 }
 
 static GtkCssProvider *
@@ -419,7 +417,7 @@ take_screenshot (const char *name,
     data->nav_view_child_widget = GTK_WIDGET (nav_view_child_widget);
 
   if (wait)
-    g_timeout_add_seconds (1, G_SOURCE_FUNC (take_screenshot_cb), data);
+    g_timeout_add_once (1000, (GSourceOnceFunc) take_screenshot_cb, data);
 
   gtk_window_present (GTK_WINDOW (window));
 