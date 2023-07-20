cb5a62bfebdf54e76007d023ebb2853d4134235c
Alice Mikhaylenko <alicem@gnome.org>
2023-07-20 07:57:01 +0400

    leaflet: Fix mouse button handling
    Fixes https://gitlab.gnome.org/GNOME/libadwaita/-/issues/370
    
    
    (cherry picked from commit 9823ca9550e83885b4cae011a56173cc39ba9d08)
    
    (cherry picked from commit 0fea5e3b3539a4f34bd3f8a4596a4d8c5194eb82)

--- src/adw-leaflet.c
+++ src/adw-leaflet.c
@@ -1422,11 +1422,6 @@ back_forward_button_pressed_cb (GtkGesture *gesture,
   guint button;
   AdwNavigationDirection direction;
 
-  if (n_press > 1) {
-    gtk_gesture_set_state (gesture, GTK_EVENT_SEQUENCE_DENIED);
-    return;
-  }
-
   button = gtk_gesture_single_get_current_button (GTK_GESTURE_SINGLE (gesture));
 
   /* Unfortunately, there are no constants for these buttons */
@@ -1436,6 +1431,7 @@ back_forward_button_pressed_cb (GtkGesture *gesture,
     direction = ADW_NAVIGATION_DIRECTION_FORWARD;
   } else {
     gtk_gesture_set_state (gesture, GTK_EVENT_SEQUENCE_DENIED);
+    gtk_event_controller_reset (GTK_EVENT_CONTROLLER (gesture));
     return;
   }
 
