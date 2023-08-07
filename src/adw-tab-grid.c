GNOME/libadwaita

commit 1452b1a688328f8a295f0613f01230d22275e8bb

tab-overview: Cull invisible thumbnails

src/adw-tab-grid.c
@@ -189,6 +189,8 @@ struct _AdwTabGrid
   double visible_lower;
   double visible_upper;
   double page_size;
+  double lower_inset;
+  double upper_inset;
 
   GtkStringFilter *title_filter;
   GtkStringFilter *tooltip_filter;
@@ -3206,6 +3208,30 @@ adw_tab_grid_direction_changed (GtkWidget        *widget,
 }
 
 static void
+adw_tab_grid_snapshot (GtkWidget   *widget,
+                       GtkSnapshot *snapshot)
+{
+  AdwTabGrid *self = ADW_TAB_GRID (widget);
+  GList *l;
+
+  for (l = self->tabs; l; l = l->next) {
+    TabInfo *info = l->data;
+    int pos, height;
+
+    pos = get_tab_y (self, info, FALSE);
+    height = gtk_widget_get_height (info->container);
+
+    if (pos + height < self->visible_lower - self->lower_inset)
+      continue;
+
+    if (pos > self->visible_upper + self->upper_inset)
+      continue;
+
+    gtk_widget_snapshot_child (widget, info->container, snapshot);
+  }
+}
+
+static void
 adw_tab_grid_dispose (GObject *object)
 {
   AdwTabGrid *self = ADW_TAB_GRID (object);
@@ -3317,6 +3343,7 @@ adw_tab_grid_class_init (AdwTabGridClass *klass)
   widget_class->unrealize = adw_tab_grid_unrealize;
   widget_class->unmap = adw_tab_grid_unmap;
   widget_class->direction_changed = adw_tab_grid_direction_changed;
+  widget_class->snapshot = adw_tab_grid_snapshot;
 
   props[PROP_PINNED] =
     g_param_spec_boolean ("pinned", NULL, NULL,
@@ -3683,13 +3710,17 @@ void
 adw_tab_grid_set_visible_range (AdwTabGrid *self,
                                 double      lower,
                                 double      upper,
-                                double      page_size)
+                                double      page_size,
+                                double      lower_inset,
+                                double      upper_inset)
 {
   g_return_if_fail (ADW_IS_TAB_GRID (self));
 
   self->visible_lower = lower;
   self->visible_upper = upper;
   self->page_size = page_size;
+  self->lower_inset = lower_inset;
+  self->upper_inset = upper_inset;
 
   gtk_widget_queue_allocate (GTK_WIDGET (self));
 }

src/adw-tab-grid-private.h
@@ -56,7 +56,9 @@ AdwTabThumbnail *adw_tab_grid_get_transition_thumbnail (AdwTabGrid *self);
 void adw_tab_grid_set_visible_range (AdwTabGrid *self,
                                      double      lower,
                                      double      upper,
-                                     double      page_size);
+                                     double      page_size,
+                                     double      lower_inset,
+                                     double      upper_inset);
 
 void adw_tab_grid_adjustment_shifted (AdwTabGrid *self,
                                       double      delta);

src/adw-tab-overview.c
@@ -534,11 +534,15 @@ adw_tab_overview_scrollable_size_allocate (GtkWidget *widget,
   adw_tab_grid_set_visible_range (ADW_TAB_GRID (self->pinned_grid),
                                   CLAMP (value - self->pinned_grid_pos, 0, pinned_height),
                                   CLAMP (value - self->pinned_grid_pos + height - new_button_height, 0, pinned_height),
-                                  height - new_button_height);
+                                  height - new_button_height,
+                                  0,
+                                  CLAMP (self->pinned_grid_pos + pinned_height - height + new_button_height - value, 0, new_button_height));
   adw_tab_grid_set_visible_range (ADW_TAB_GRID (self->grid),
                                   CLAMP (value - self->grid_pos, 0, grid_height),
                                   CLAMP (value - self->grid_pos + height - new_button_height, 0, grid_height),
-                                  height - new_button_height);
+                                  height - new_button_height,
+                                  0,
+                                  CLAMP (self->grid_pos + grid_height - height + new_button_height - value, 0, new_button_height));
 
   if (self->scroll_animation_done) {
     g_clear_pointer (&self->scroll_animation_grid, adw_tab_grid_reset_scrolled_tab);

commit ef4364022a7f99c70b1a325ae23e4e3ad0ebf0ed

tab-grid: Snapshot reordered thumbnail after others
Fix a fallout from 1452b1a688328f8a295f0613f01230d22275e8bb

src/adw-tab-grid.c
@@ -3218,6 +3218,9 @@ adw_tab_grid_snapshot (GtkWidget   *widget,
     TabInfo *info = l->data;
     int pos, height;
 
+    if (info == self->reordered_tab)
+      continue;
+
     pos = get_tab_y (self, info, FALSE);
     height = gtk_widget_get_height (info->container);
 
@@ -3229,6 +3232,9 @@ adw_tab_grid_snapshot (GtkWidget   *widget,
 
     gtk_widget_snapshot_child (widget, info->container, snapshot);
   }
+
+  if (self->reordered_tab)
+    gtk_widget_snapshot_child (widget, self->reordered_tab->container, snapshot);
 }
 
 static void