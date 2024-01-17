16cb98698634f1e5ae93f60e016315331a0c444d
Alice Mikhaylenko <alicem@gnome.org>
2024-01-17 18:30:38 +0400

    demo: Port view switcher demo to AdwDialog

--- demo/pages/view-switcher/adw-view-switcher-demo-window.c
+++ demo/pages/view-switcher/adw-view-switcher-demo-window.c
@@ -4,10 +4,10 @@
 
 struct _AdwViewSwitcherDemoWindow
 {
-  AdwWindow parent_instance;
+  AdwDialog parent_instance;
 };
 
-G_DEFINE_FINAL_TYPE (AdwViewSwitcherDemoWindow, adw_view_switcher_demo_window, ADW_TYPE_WINDOW)
+G_DEFINE_FINAL_TYPE (AdwViewSwitcherDemoWindow, adw_view_switcher_demo_window, ADW_TYPE_DIALOG)
 
 static void
 adw_view_switcher_demo_window_class_init (AdwViewSwitcherDemoWindowClass *klass)
@@ -15,8 +15,6 @@ adw_view_switcher_demo_window_class_init (AdwViewSwitcherDemoWindowClass *klass)
   GtkWidgetClass *widget_class = GTK_WIDGET_CLASS (klass);
 
   gtk_widget_class_set_template_from_resource (widget_class, "/org/gnome/Adwaita1/Demo/ui/pages/view-switcher/adw-view-switcher-demo-window.ui");
-
-  gtk_widget_class_add_binding_action (widget_class, GDK_KEY_Escape, 0, "window.close", NULL);
 }
 
 static void
--- demo/pages/view-switcher/adw-view-switcher-demo-window.h
+++ demo/pages/view-switcher/adw-view-switcher-demo-window.h
@@ -6,7 +6,7 @@ G_BEGIN_DECLS
 
 #define ADW_TYPE_VIEW_SWITCHER_DEMO_WINDOW (adw_view_switcher_demo_window_get_type())
 
-G_DECLARE_FINAL_TYPE (AdwViewSwitcherDemoWindow, adw_view_switcher_demo_window, ADW, VIEW_SWITCHER_DEMO_WINDOW, AdwWindow)
+G_DECLARE_FINAL_TYPE (AdwViewSwitcherDemoWindow, adw_view_switcher_demo_window, ADW, VIEW_SWITCHER_DEMO_WINDOW, AdwDialog)
 
 AdwViewSwitcherDemoWindow *adw_view_switcher_demo_window_new (void);
 
