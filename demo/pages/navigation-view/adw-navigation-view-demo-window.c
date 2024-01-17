c71ee9068e767df946f09d613caf416967ba3d44
Alice Mikhaylenko <alicem@gnome.org>
2024-01-17 18:31:36 +0400

    demo: Port navigation view demo to AdwDialog

--- demo/pages/navigation-view/adw-navigation-view-demo-window.c
+++ demo/pages/navigation-view/adw-navigation-view-demo-window.c
@@ -4,10 +4,10 @@
 
 struct _AdwNavigationViewDemoWindow
 {
-  AdwWindow parent_instance;
+  AdwDialog parent_instance;
 };
 
-G_DEFINE_FINAL_TYPE (AdwNavigationViewDemoWindow, adw_navigation_view_demo_window, ADW_TYPE_WINDOW)
+G_DEFINE_FINAL_TYPE (AdwNavigationViewDemoWindow, adw_navigation_view_demo_window, ADW_TYPE_DIALOG)
 
 static void
 adw_navigation_view_demo_window_class_init (AdwNavigationViewDemoWindowClass *klass)
@@ -15,8 +15,6 @@ adw_navigation_view_demo_window_class_init (AdwNavigationViewDemoWindowClass *kl
   GtkWidgetClass *widget_class = GTK_WIDGET_CLASS (klass);
 
   gtk_widget_class_set_template_from_resource (widget_class, "/org/gnome/Adwaita1/Demo/ui/pages/navigation-view/adw-navigation-view-demo-window.ui");
-
-  gtk_widget_class_add_binding_action (widget_class, GDK_KEY_Escape, 0, "window.close", NULL);
 }
 
 static void
--- demo/pages/navigation-view/adw-navigation-view-demo-window.h
+++ demo/pages/navigation-view/adw-navigation-view-demo-window.h
@@ -6,7 +6,7 @@ G_BEGIN_DECLS
 
 #define ADW_TYPE_NAVIGATION_VIEW_DEMO_WINDOW (adw_navigation_view_demo_window_get_type())
 
-G_DECLARE_FINAL_TYPE (AdwNavigationViewDemoWindow, adw_navigation_view_demo_window, ADW, NAVIGATION_VIEW_DEMO_WINDOW, AdwWindow)
+G_DECLARE_FINAL_TYPE (AdwNavigationViewDemoWindow, adw_navigation_view_demo_window, ADW, NAVIGATION_VIEW_DEMO_WINDOW, AdwDialog)
 
 AdwNavigationViewDemoWindow *adw_navigation_view_demo_window_new (void);
 
