c433fe34809f68e24491ab1d974ac38d584b3a36
𝅳Automeris naranja <104251-automerisnaranja@users.noreply.gitlab.gnome.org>
2024-04-18 12:16:30 +0000

    action-row: Mention property rows in docs
    (cherry picked from commit 5e25e152ef6406c6a896a20d49adb46ecf7bd266)
    
    89e456c7 docs: Add link about property rows in AdwActionRow doc page
    
    (cherry picked from commit abaf253edbdfbbdc4418f82ad8e01e77c4ebae98)

--- src/adw-action-row.c
+++ src/adw-action-row.c
@@ -47,6 +47,11 @@
  *
  * It contains subnodes `label.title` and `label.subtitle` representing
  * respectively the title label and subtitle label.
+ *
+ * `AdwActionRow` can use the
+ * [`.property`](style-classes.html#property-rows) style class to emphasize
+ * the row subtitle instead of the row title, which is useful for
+ * displaying read-only properties.
  */
 
 typedef struct
