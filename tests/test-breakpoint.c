53e03ae6011639c091eba61f33608b500fbbe12f
Zander Brown <zbrown@gnome.org>
2024-05-08 11:51:04 +0100

    breakpoint: don't use expect_message()
    This one is a little more involved, with what was originally a single
    ‘test’ now split into 58, since each one could in theory abort…

--- tests/test-breakpoint.c
+++ tests/test-breakpoint.c
@@ -20,35 +20,6 @@ check_to_string (AdwBreakpointCondition *condition,
   adw_breakpoint_condition_free (condition);
 }
 
-static inline void
-check_parse (const char *input,
-             const char *expected)
-{
-  AdwBreakpointCondition *condition;
-
-  if (!expected)
-    g_test_expect_message (ADW_LOG_DOMAIN, G_LOG_LEVEL_CRITICAL, "*Unable to parse condition*");
-
-  condition = adw_breakpoint_condition_parse (input);
-
-  if (expected) {
-    g_assert_nonnull (condition);
-
-    check_to_string (condition, expected);
-  } else {
-    if (condition != NULL) {
-      char *str = adw_breakpoint_condition_to_string (condition);
-
-      g_test_message ("'%s' is invalid, but was parsed as '%s'", input, str);
-      g_free (str);
-    }
-
-    g_test_assert_expected_messages ();
-
-    g_assert_null (condition);
-  }
-}
-
 static void
 test_adw_breakpoint_condition_to_string (void)
 {
@@ -111,91 +82,130 @@ test_adw_breakpoint_condition_to_string (void)
   adw_breakpoint_condition_free (condition_3);
 }
 
-static void
-test_adw_breakpoint_condition_parse (void)
-{
-  check_parse ("", NULL);
-  check_parse ("()", NULL);
-  check_parse ("foo", NULL);
+static struct parse_test {
+  const char *input;
+  const char *expected;
+} parse_tests[] = {
+  { "", NULL },
+  { "()", NULL },
+  { "foo", NULL },
 
   /* Length */
-  check_parse ("max-width: 400px",   "max-width: 400px");
-  check_parse ("max-width: 400",     "max-width: 400px");
-  check_parse ("max-width: 400pt",   "max-width: 400pt");
-  check_parse ("max-width:400pt",    "max-width: 400pt");
-  check_parse ("max-width: 400.0px", "max-width: 400px");
-  check_parse ("max-width: 400.5px", "max-width: 400.5px");
-  check_parse ("      max-width        :        400     pt       ", "max-width: 400pt");
+  { "max-width: 400px",   "max-width: 400px" },
+  { "max-width: 400",     "max-width: 400px" },
+  { "max-width: 400pt",   "max-width: 400pt" },
+  { "max-width:400pt",    "max-width: 400pt" },
+  { "max-width: 400.0px", "max-width: 400px" },
+  { "max-width: 400.5px", "max-width: 400.5px" },
+  { "      max-width        :        400     pt       ", "max-width: 400pt" },
 
-  check_parse ("max-width:",        NULL);
-  check_parse ("max-width: px",     NULL);
-  check_parse ("max-length: 400px", NULL);
-  check_parse ("max-width 400px",   NULL);
-  check_parse ("max-width: -1px",   NULL);
-  check_parse ("max-width: 400p",   NULL);
-  check_parse ("max-width: 400px;", NULL);
+  { "max-width:",        NULL },
+  { "max-width: px",     NULL },
+  { "max-length: 400px", NULL },
+  { "max-width 400px",   NULL },
+  { "max-width: -1px",   NULL },
+  { "max-width: 400p",   NULL },
+  { "max-width: 400px;", NULL },
 
   /* Ratio */
-  check_parse ("max-aspect-ratio: 4/3", "max-aspect-ratio: 4/3");
-  check_parse ("max-aspect-ratio: 2",   "max-aspect-ratio: 2");
-  check_parse ("max-aspect-ratio: 2/1", "max-aspect-ratio: 2");
-  check_parse ("max-aspect-ratio: 0/3", "max-aspect-ratio: 0");
-  check_parse ("max-aspect-ratio:4/3",  "max-aspect-ratio: 4/3");
-  check_parse ("       max-aspect-ratio   :         4/3       ", "max-aspect-ratio: 4/3");
+  { "max-aspect-ratio: 4/3", "max-aspect-ratio: 4/3" },
+  { "max-aspect-ratio: 2",   "max-aspect-ratio: 2" },
+  { "max-aspect-ratio: 2/1", "max-aspect-ratio: 2" },
+  { "max-aspect-ratio: 0/3", "max-aspect-ratio: 0" },
+  { "max-aspect-ratio:4/3",  "max-aspect-ratio: 4/3" },
+  { "       max-aspect-ratio   :         4/3       ", "max-aspect-ratio: 4/3" },
 
-  check_parse ("max-aspect-ratio:",       NULL);
-  check_parse ("max-aspect-ratio: 4/3px", NULL);
-  check_parse ("max-aspect-ratio: 4px",   NULL);
-  check_parse ("max-aspect-ratio: -4",    NULL);
-  check_parse ("max-aspect-ratio: -4/3",  NULL);
-  check_parse ("max-aspect-ratio: 4/0",   NULL);
-  check_parse ("max-aspect-ratio: 4/3;",  NULL);
+  { "max-aspect-ratio:",       NULL },
+  { "max-aspect-ratio: 4/3px", NULL },
+  { "max-aspect-ratio: 4px",   NULL },
+  { "max-aspect-ratio: -4",    NULL },
+  { "max-aspect-ratio: -4/3",  NULL },
+  { "max-aspect-ratio: 4/0",   NULL },
+  { "max-aspect-ratio: 4/3;",  NULL },
 
   /* Single + parentheses */
-  check_parse ("(max-width: 100px)",     "max-width: 100px");
-  check_parse ("(((max-width: 100px)))", "max-width: 100px");
-  check_parse ("   (   max-width   :   100px   )   ", "max-width: 100px");
+  { "(max-width: 100px)",     "max-width: 100px" },
+  { "(((max-width: 100px)))", "max-width: 100px" },
+  { "   (   max-width   :   100px   )   ", "max-width: 100px" },
 
-  check_parse ("(max-width: 100px",   NULL);
-  check_parse ("(max-width: 100px(",  NULL);
-  check_parse ("(max-width): 100px",  NULL);
-  check_parse ("(max-width: 100px))", NULL);
+  { "(max-width: 100px",   NULL },
+  { "(max-width: 100px(",  NULL },
+  { "(max-width): 100px",  NULL },
+  { "(max-width: 100px))", NULL },
 
   /* Multi */
-  check_parse ("max-width: 100px and max-height: 200px", "max-width: 100px and max-height: 200px");
-  check_parse ("max-width: 100px or max-height: 200px",  "max-width: 100px or max-height: 200px");
-  check_parse ("   max-width   :   100px   or   max-height   :   200px   ", "max-width: 100px or max-height: 200px");
+  { "max-width: 100px and max-height: 200px", "max-width: 100px and max-height: 200px" },
+  { "max-width: 100px or max-height: 200px",  "max-width: 100px or max-height: 200px" },
+  { "   max-width   :   100px   or   max-height   :   200px   ", "max-width: 100px or max-height: 200px" },
 
-  check_parse ("(max-width: 100px) and max-height: 200px",   "max-width: 100px and max-height: 200px");
-  check_parse ("max-width: 100px and (max-height: 200px)",   "max-width: 100px and max-height: 200px");
-  check_parse ("(max-width: 100px) and (max-height: 200px)", "max-width: 100px and max-height: 200px");
-  check_parse ("(max-width: 100px and max-height: 200px)",   "max-width: 100px and max-height: 200px");
+  { "(max-width: 100px) and max-height: 200px",   "max-width: 100px and max-height: 200px" },
+  { "max-width: 100px and (max-height: 200px)",   "max-width: 100px and max-height: 200px" },
+  { "(max-width: 100px) and (max-height: 200px)", "max-width: 100px and max-height: 200px" },
+  { "(max-width: 100px and max-height: 200px)",   "max-width: 100px and max-height: 200px" },
 
-  check_parse ("(max-width: 100px and max-height: 200px) or max-aspect-ratio: 3/2",
-               "(max-width: 100px and max-height: 200px) or max-aspect-ratio: 3/2");
-  check_parse ("max-width: 100px and (max-height: 200px or max-aspect-ratio: 3/2)",
-               "max-width: 100px and (max-height: 200px or max-aspect-ratio: 3/2)");
+  { "(max-width: 100px and max-height: 200px) or max-aspect-ratio: 3/2",
+               "(max-width: 100px and max-height: 200px) or max-aspect-ratio: 3/2" },
+  { "max-width: 100px and (max-height: 200px or max-aspect-ratio: 3/2)",
+               "max-width: 100px and (max-height: 200px or max-aspect-ratio: 3/2)" },
 
-  check_parse ("(max-width: 100px and max-height: 200px) or max-aspect-ratio: 3/2",
-               "(max-width: 100px and max-height: 200px) or max-aspect-ratio: 3/2");
-  check_parse ("max-width: 100px and (max-height: 200px or max-aspect-ratio: 3/2)",
-               "max-width: 100px and (max-height: 200px or max-aspect-ratio: 3/2)");
+  { "(max-width: 100px and max-height: 200px) or max-aspect-ratio: 3/2",
+               "(max-width: 100px and max-height: 200px) or max-aspect-ratio: 3/2" },
+  { "max-width: 100px and (max-height: 200px or max-aspect-ratio: 3/2)",
+               "max-width: 100px and (max-height: 200px or max-aspect-ratio: 3/2)" },
 
-  check_parse ("max-width: 100px and max-height: 200px and max-aspect-ratio: 3/2 and min-aspect-ratio: 1/2",
-               "max-width: 100px and max-height: 200px and max-aspect-ratio: 3/2 and min-aspect-ratio: 1/2");
-  check_parse ("max-width: 100px or max-height: 200px or max-aspect-ratio: 3/2 or min-aspect-ratio: 1/2",
-               "max-width: 100px or max-height: 200px or max-aspect-ratio: 3/2 or min-aspect-ratio: 1/2");
+  { "max-width: 100px and max-height: 200px and max-aspect-ratio: 3/2 and min-aspect-ratio: 1/2",
+               "max-width: 100px and max-height: 200px and max-aspect-ratio: 3/2 and min-aspect-ratio: 1/2" },
+  { "max-width: 100px or max-height: 200px or max-aspect-ratio: 3/2 or min-aspect-ratio: 1/2",
+               "max-width: 100px or max-height: 200px or max-aspect-ratio: 3/2 or min-aspect-ratio: 1/2" },
 
-  check_parse ("max-width: 100px and max-height: 200px or max-aspect-ratio: 3/2 and min-aspect-ratio: 1/2",
-               "((max-width: 100px and max-height: 200px) or max-aspect-ratio: 3/2) and min-aspect-ratio: 1/2");
+  { "max-width: 100px and max-height: 200px or max-aspect-ratio: 3/2 and min-aspect-ratio: 1/2",
+               "((max-width: 100px and max-height: 200px) or max-aspect-ratio: 3/2) and min-aspect-ratio: 1/2" },
 
-  check_parse ("max-width: 100pxor max-height: 200px", NULL);
-  check_parse ("max-width: 100px ormax-height: 200px", NULL);
-  check_parse ("max-width: 100px max-height: 200px",   NULL);
-  check_parse ("max-width: 100px or max-height",       NULL);
-  check_parse ("max-width: 100px o",                   NULL);
-  check_parse ("max-width: 100px or ()",               NULL);
-  check_parse ("() or max-height: 200px",              NULL);
+  { "max-width: 100pxor max-height: 200px", NULL },
+  { "max-width: 100px ormax-height: 200px", NULL },
+  { "max-width: 100px max-height: 200px",   NULL },
+  { "max-width: 100px or max-height",       NULL },
+  { "max-width: 100px o",                   NULL },
+  { "max-width: 100px or ()",               NULL },
+  { "() or max-height: 200px",              NULL },
+};
+
+static void
+test_adw_breakpoint_condition_parse (gconstpointer user_data)
+{
+  const struct parse_test *test = user_data;
+
+  if (g_test_subprocess ()) {
+    AdwBreakpointCondition *condition;
+
+    condition = adw_breakpoint_condition_parse (test->input);
+
+    if (test->expected) {
+      g_assert_nonnull (condition);
+
+      check_to_string (condition, test->expected);
+    } else {
+      if (condition != NULL) {
+        char *str = adw_breakpoint_condition_to_string (condition);
+
+        g_test_message ("'%s' is invalid, but was parsed as '%s'", test->input, str);
+        g_free (str);
+      }
+
+      g_assert_null (condition);
+    }
+
+    return;
+  }
+
+  g_test_trap_subprocess (NULL, 0, 0);
+
+  if (test->expected) {
+    g_test_trap_assert_passed ();
+  } else {
+    g_test_trap_assert_failed ();
+    g_test_trap_assert_stderr ("*Unable to parse condition*");
+  }
 }
 
 int
@@ -206,7 +216,12 @@ main (int   argc,
   adw_init ();
 
   g_test_add_func ("/Adwaita/BreakpointCondition/to_string", test_adw_breakpoint_condition_to_string);
-  g_test_add_func ("/Adwaita/BreakpointCondition/parse", test_adw_breakpoint_condition_parse);
+
+  for (size_t i = 0; i < G_N_ELEMENTS (parse_tests); i++) {
+    char *path = g_strdup_printf ("/Adwaita/BreakpointCondition/parse_%li", i);
+    g_test_add_data_func (path, &parse_tests[i], test_adw_breakpoint_condition_parse);
+    g_free (path);
+  }
 
   return g_test_run ();
 }
