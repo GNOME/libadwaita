ca91528a823bfa27a871d5988fbcd4e624f6752c
Alice Mikhaylenko <alicem@gnome.org>
2024-08-27 18:20:00 +0400

    tab-bar: Add style classes docs

--- src/adw-tab-bar.c
+++ src/adw-tab-bar.c
@@ -37,6 +37,18 @@
  * ## CSS nodes
  *
  * `AdwTabBar` has a single CSS node with name `tabbar`.
+ *
+ * ## Style classes
+ *
+ * By default `AdwTabBar` look like a part of an `AdwHeaderBar` and is intended
+ * to be used directly attached to one or used as a [class@ToolbarView] toolbar.
+ * The [`.inline`](style-classes.html#inline) style class removes its background,
+ * so that it can be used in different contexts instead.
+ *
+ * <picture>
+ *   <source srcset="tab-bar-inline-dark.png" media="(prefers-color-scheme: dark)">
+ *   <img src="tab-bar-inline.png" alt="tab-bar-inline">
+ * </picture>
  */
 
 struct _AdwTabBar
