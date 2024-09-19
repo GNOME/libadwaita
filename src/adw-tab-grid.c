66ba72b22864f38c36c542bfdfaf0ae469fc5ae7
Alice Mikhaylenko <alicem@gnome.org>
2024-09-19 19:55:34 +0400

    tab-box/grid: Skip middle clicks if hovering different tab between press/release
    Fixes https://gitlab.gnome.org/GNOME/libadwaita/-/issues/915
    
    
    (cherry picked from commit 9079584dd997200a60f046b26949fc761748ce13)
    
    (cherry picked from commit 21739d348029b53227dd47c82944ce9e3dd7e771)

--- src/adw-tab-grid.c
+++ src/adw-tab-grid.c
@@ -199,6 +199,8 @@ struct _AdwTabGrid
   gboolean searching;
 
   gboolean empty;
+
+  TabInfo *middle_clicked_tab;
 };
 
 G_DEFINE_FINAL_TYPE (AdwTabGrid, adw_tab_grid, GTK_TYPE_WIDGET)
@@ -1945,6 +1947,9 @@ close_animation_done_cb (TabInfo *info)
   if (self->reordered_tab == info)
     self->reordered_tab = NULL;
 
+  if (self->middle_clicked_tab == info)
+    self->pressed_tab = NULL;
+
   remove_and_free_tab_info (info);
 
   self->n_tabs--;
@@ -2925,6 +2930,7 @@ pressed_cb (AdwTabGrid *self,
   button = gtk_gesture_single_get_current_button (GTK_GESTURE_SINGLE (gesture));
 
   if (button == GDK_BUTTON_MIDDLE) {
+    self->middle_clicked_tab = info;
     gtk_gesture_set_state (gesture, GTK_EVENT_SEQUENCE_CLAIMED);
 
     return;
@@ -2964,7 +2970,14 @@ released_cb (AdwTabGrid *self,
   button = gtk_gesture_single_get_current_button (GTK_GESTURE_SINGLE (gesture));
 
   if (button == GDK_BUTTON_MIDDLE) {
+    if (info != self->middle_clicked_tab) {
+      self->middle_clicked_tab = NULL;
+      gtk_gesture_set_state (gesture, GTK_EVENT_SEQUENCE_DENIED);
+      return;
+    }
+
     adw_tab_view_close_page (self->view, info->page);
+    self->middle_clicked_tab = NULL;
 
     return;
   }
