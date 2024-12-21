9d2808d1ee327012d60146bbf63b0ed1e73780d2
Alice Mikhaylenko <alicem@gnome.org>
2024-12-21 17:27:04 +0400

    adaptive-preview: Round coordinates for the scale bin child
    Avoid blurriness on integer scales at least.

--- src/adw-adaptive-preview.c
+++ src/adw-adaptive-preview.c
@@ -15,6 +15,7 @@
 #include "adw-combo-row.h"
 #include "adw-marshalers.h"
 #include "adw-widget-utils-private.h"
+#include <math.h>
 
 #define MIN_SCALE 0.25
 #define MAX_SCALE 1.0
@@ -435,12 +436,12 @@ allocate_scale_bin (GtkWidget *widget,
   }
 
   transform = gsk_transform_translate (transform,
-                                       &GRAPHENE_POINT_INIT (width / 2.0f,
-                                                             height / 2.0f));
+                                       &GRAPHENE_POINT_INIT (roundf (width / 2.0f),
+                                                             roundf (height / 2.0f)));
   transform = gsk_transform_scale (transform, scale, scale);
   transform = gsk_transform_translate (transform,
-                                       &GRAPHENE_POINT_INIT (-child_width / 2.0f,
-                                                             -child_height / 2.0f));
+                                       &GRAPHENE_POINT_INIT (-roundf (child_width / 2.0f),
+                                                             -roundf (child_height / 2.0f)));
 
   gtk_widget_allocate (child, child_width, child_height, -1, transform);
 }
