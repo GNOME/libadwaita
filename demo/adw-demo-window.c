625a232f577bce2acfb83d3f269b4fee43dc91f8
Alice Mikhaylenko <alicem@gnome.org>
2025-09-26 20:11:32 +0400

    demo: Drop the color scheme toggle
    By now everyone should have the portal.

--- demo/adw-demo-window.c
+++ demo/adw-demo-window.c
@@ -28,42 +28,11 @@ struct _AdwDemoWindow
 {
   AdwApplicationWindow parent_instance;
 
-  GtkWidget *color_scheme_button;
   AdwNavigationSplitView *split_view;
   AdwSidebar *sidebar;
 };
 
 G_DEFINE_FINAL_TYPE (AdwDemoWindow, adw_demo_window, ADW_TYPE_APPLICATION_WINDOW)
-
-static char *
-get_color_scheme_icon_name (gpointer user_data,
-                            gboolean dark)
-{
-  return g_strdup (dark ? "light-mode-symbolic" : "dark-mode-symbolic");
-}
-
-static void
-color_scheme_button_clicked_cb (AdwDemoWindow *self)
-{
-  AdwStyleManager *manager = adw_style_manager_get_default ();
-
-  if (adw_style_manager_get_dark (manager))
-    adw_style_manager_set_color_scheme (manager, ADW_COLOR_SCHEME_FORCE_LIGHT);
-  else
-    adw_style_manager_set_color_scheme (manager, ADW_COLOR_SCHEME_FORCE_DARK);
-}
-
-static void
-notify_system_supports_color_schemes_cb (AdwDemoWindow *self)
-{
-  AdwStyleManager *manager = adw_style_manager_get_default ();
-  gboolean supports = adw_style_manager_get_system_supports_color_schemes (manager);
-
-  gtk_widget_set_visible (self->color_scheme_button, !supports);
-
-  if (supports)
-    adw_style_manager_set_color_scheme (manager, ADW_COLOR_SCHEME_DEFAULT);
-}
 
 static void
 update_content (AdwDemoWindow *self)
@@ -118,11 +87,8 @@ adw_demo_window_class_init (AdwDemoWindowClass *klass)
 #endif
 
   gtk_widget_class_set_template_from_resource (widget_class, "/org/gnome/Adwaita1/Demo/ui/adw-demo-window.ui");
-  gtk_widget_class_bind_template_child (widget_class, AdwDemoWindow, color_scheme_button);
   gtk_widget_class_bind_template_child (widget_class, AdwDemoWindow, split_view);
   gtk_widget_class_bind_template_child (widget_class, AdwDemoWindow, sidebar);
-  gtk_widget_class_bind_template_callback (widget_class, get_color_scheme_icon_name);
-  gtk_widget_class_bind_template_callback (widget_class, color_scheme_button_clicked_cb);
   gtk_widget_class_bind_template_callback (widget_class, sidebar_activated_cb);
 
   gtk_widget_class_install_action (widget_class, "window.adaptive-preview", NULL, (GtkWidgetActionActivateFunc) adaptive_preview_cb);
@@ -158,14 +124,6 @@ adw_demo_window_init (AdwDemoWindow *self)
   g_type_ensure (ADW_TYPE_DEMO_PAGE_WRAP_BOX);
 
   gtk_widget_init_template (GTK_WIDGET (self));
-
-  g_signal_connect_object (manager,
-                           "notify::system-supports-color-schemes",
-                           G_CALLBACK (notify_system_supports_color_schemes_cb),
-                           self,
-                           G_CONNECT_SWAPPED);
-
-  notify_system_supports_color_schemes_cb (self);
 
   update_content (self);
 }
