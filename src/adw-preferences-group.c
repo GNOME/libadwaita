54dd567a431a3713238597fac7eb1b1bc7eb72db
Alice <alicem@gnome.org>
2025-11-21 02:14:37 +0400

    preferences-group: Fix title an description getters
    See https://gitlab.gnome.org/GNOME/libadwaita/-/issues/1098
    
    
    (cherry picked from commit c9ab421a1acde1e935fa54d8af79b9169b250f3a)
    
    Co-authored-by: Alice Mikhaylenko <alicem@gnome.org>
    
    (cherry picked from commit 7c7abfdfbd7c1a61ccbca9fef1271658bd51d10b)
    
    Co-authored-by: Alice <alicem@gnome.org>

--- src/adw-preferences-group.c
+++ src/adw-preferences-group.c
@@ -459,7 +459,7 @@ adw_preferences_group_get_title (AdwPreferencesGroup *self)
 
   priv = adw_preferences_group_get_instance_private (self);
 
-  return gtk_label_get_text (priv->title);
+  return gtk_label_get_label (priv->title);
 }
 
 /**
@@ -506,7 +506,7 @@ adw_preferences_group_get_description (AdwPreferencesGroup *self)
 
   priv = adw_preferences_group_get_instance_private (self);
 
-  return gtk_label_get_text (priv->description);
+  return gtk_label_get_label (priv->description);
 }
 
 /**
