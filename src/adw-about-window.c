abf8e5a5a9a27dce77f0f73a5d739f665e7d7310
Alice <alicem@gnome.org>
2026-04-15 22:23:17 +0400

    about-dialog/window: Speed up adding credit sections
    Same as last commit.
    
    
    (cherry picked from commit ca193b081361c9fb228961cebe06c10f9681bf83)
    
    Co-authored-by: Alice Mikhaylenko <alicem@gnome.org>

--- src/adw-about-window.c
+++ src/adw-about-window.c
@@ -3140,7 +3140,11 @@ adw_about_window_add_credit_section (AdwAboutWindow  *self,
 
   self->credit_sections = g_slist_append (self->credit_sections, section);
 
-  update_credits (self);
+  add_credits_section (self, self->credits_box, section->name, section->people);
+
+  gtk_widget_set_visible (self->credits_box, TRUE);
+
+  update_credits_legal_group (self);
 }
 
 /**
